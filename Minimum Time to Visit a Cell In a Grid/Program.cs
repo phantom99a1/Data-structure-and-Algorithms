28cbb4a179e6cb80795b6536feadd8183af1f133
phantom99a1 <viendanho99a1@gmail.com>
2024-11-30 20:37:52 +0700

    Add description of problem

--- Minimum Time to Visit a Cell In a Grid/Program.cs
+++ Minimum Time to Visit a Cell In a Grid/Program.cs
@@ -1,5 +1,14 @@
 ﻿namespace Minimum_Time_to_Visit_a_Cell_In_a_Grid
 {
+    /*You are given a m x n matrix grid consisting of non-negative integers where 
+     * grid[row][col] represents the minimum time required to be able to visit the cell (row, col), 
+     * which means you can visit the cell (row, col) only when the time you visit it is greater than or equal to grid[row][col].
+
+    You are standing in the top-left cell of the matrix in the 0th second, 
+    and you must move to any adjacent cell in the four directions: up, down, left, and right. Each move you make takes 1 second.
+
+    Return the minimum time required in which you can visit the bottom-right cell of the matrix. 
+    If you cannot visit the bottom-right cell, then return -1.*/
     public class Solution
     {
         private static readonly (int dr, int dc)[] MOVES = [(-1, 0), (0, 1), (1, 0), (0, -1)];
