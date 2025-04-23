4cc7b478cb3043e0cf67c5f097ec5cee31512da2
phantom99a1 <viendanho99a1@gmail.com>
2025-04-23 20:20:31 +0700

    Add the problem Count Largest Group

---
+++ Count Largest Group/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Count_Largest_Group
+{
+    /*You are given an integer n. Each number from 1 to n is grouped according to the sum of its digits.
+    Return the number of groups that have the largest size.
+    
+     Constraint:
+    1 <= n <= 10^4
+     */
+
+    public class Solution
+    {
+        public int CountLargestGroup(int n)
+        {
+            Dictionary<int, int> digitSumCount = new Dictionary<int, int>();
+            int maxSize = 0, largestGroupCount = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = GetDigitSum(i);
+                if (!digitSumCount.ContainsKey(sum))
+                {
+                    digitSumCount[sum] = 0;
+                }
+                digitSumCount[sum]++;
+                maxSize = Math.Max(maxSize, digitSumCount[sum]);
+            }
+
+            foreach (var count in digitSumCount.Values)
+            {
+                if (count == maxSize) largestGroupCount++;
+            }
+
+            return largestGroupCount;
+        }
+
+        private int GetDigitSum(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
