aa69699bd5f8ddf06ecd2b57c98e40a21828b903
phantom99a1 <viendanho99a1@gmail.com>
2025-04-27 09:30:03 +0700

    Add the problem Count Subarrays of Length Three With a Condition

---
+++ Count Subarrays of Length Three With a Condition/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Count_Subarrays_of_Length_Three_With_a_Condition
+{
+    public class Solution
+    {
+        public int CountSubarrays(int[] nums)
+        {
+            int count = 0;
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                if ((nums[i - 1] + nums[i + 1]) * 2 == nums[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
