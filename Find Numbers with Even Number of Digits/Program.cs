01c38ba21682ce7dde9d9f3f918baebf052422ad
phantom99a1 <viendanho99a1@gmail.com>
2025-04-30 09:44:44 +0700

    Add the problem Find Numbers with Even Number of Digits

---
+++ Find Numbers with Even Number of Digits/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Find_Numbers_with_Even_Number_of_Digits
+{
+    /*Given an array nums of integers, return how many of them contain an even number of digits.
+     
+    Constraint:
+    1 <= nums.length <= 500
+    1 <= nums[i] <= 10^5
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int FindNumbers(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i].ToString().Length % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
