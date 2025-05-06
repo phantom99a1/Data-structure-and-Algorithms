a0c04fddd454b722ca81f5d00df7eae01b60db3b
phantom99a1 <viendanho99a1@gmail.com>
2025-05-06 18:08:28 +0700

    Add the problem Build Array from Permutation

---
+++ Build Array from Permutation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Build_Array_from_Permutation
+{
+    /*Given a zero-based permutation nums (0-indexed), build an array ans of the same 
+     length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+    A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+    
+     Constraint:
+    1 <= nums.length <= 1000
+    0 <= nums[i] < nums.length
+    The elements in nums are distinct.
+     */
+    public class Solution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    }
+}
