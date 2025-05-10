f6b8a30c1e3a6b11cf95c3c558c766eba9e9d383
phantom99a1 <viendanho99a1@gmail.com>
2025-05-10 11:34:48 +0700

    Add the problem Minimum Equal Sum of Two Arrays After Replacing Zeros

---
+++ Minimum Equal Sum of Two Arrays After Replacing Zeros/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros
+{
+    /*You are given two arrays nums1 and nums2 consisting of positive integers. You have to replace all the 0's in both arrays 
+    with strictly positive integers such that the sum of elements of both arrays becomes equal.
+    Return the minimum equal sum you can obtain, or -1 if it is impossible.
+
+    Constraint:
+    1 <= nums1.length, nums2.length <= 10^5
+     <= nums1[i], nums2[i] <= 10^6
+    */
+    public class Solution
+    {
+        public long MinSum(int[] nums1, int[] nums2)
+        {
+            long sum1 = 0, sum2 = 0;
+            long zero1 = 0, zero2 = 0;
+
+            foreach (int i in nums1)
+            {
+                sum1 += i;
+                if (i == 0)
+                {
+                    sum1++;
+                    zero1++;
+                }
+            }
+
+            foreach (int i in nums2)
+            {
+                sum2 += i;
+                if (i == 0)
+                {
+                    sum2++;
+                    zero2++;
+                }
+            }
+
+            if ((zero1 == 0 && sum2 > sum1) || (zero2 == 0 && sum1 > sum2))
+            {
+                return -1;
+            }
+
+            return Math.Max(sum1, sum2);
+        }
+    }
+}
