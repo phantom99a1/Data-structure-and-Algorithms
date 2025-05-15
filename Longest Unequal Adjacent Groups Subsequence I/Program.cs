3e019c286391fe5fd5f7796df00644389edf6b5e
phantom99a1 <viendanho99a1@gmail.com>
2025-05-15 18:04:52 +0700

    Add the problem Longest Unequal Adjacent Groups Subsequence I

---
+++ Longest Unequal Adjacent Groups Subsequence I/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Longest_Unequal_Adjacent_Groups_Subsequence_I
+{
+    /*You are given a string array words and a binary array groups both of length n, where words[i] is associated with groups[i].
+    Your task is to select the longest alternating subsequence from words. A subsequence of words is alternating if for any 
+    two consecutive strings in the sequence, their corresponding elements in the binary array groups differ. 
+    Essentially, you are to choose strings such that adjacent elements have non-matching corresponding bits in the groups array.
+    Formally, you need to find the longest subsequence of an array of indices [0, 1, ..., n - 1] denoted as [i0, i1, ..., ik-1], 
+    such that groups[ij] != groups[ij+1] for each 0 <= j < k - 1 and then find the words corresponding to these indices.
+    Return the selected subsequence. If there are multiple answers, return any of them.
+    Note: The elements in words are distinct.
+    
+     Constraint:
+    1 <= n == words.length == groups.length <= 100
+    1 <= words[i].length <= 10
+    groups[i] is either 0 or 1.
+    words consists of distinct strings.
+    words[i] consists of lowercase English letters.
+     */
+    using System;
+    using System.Collections.Generic;
+
+    class Solution
+    {
+        public IList<string> GetLongestSubsequence(string[] words, int[] groups)
+        {
+            List<string> result = new List<string> { words[0] };
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (groups[i] != groups[i - 1])
+                {
+                    result.Add(words[i]);
+                }
+            }
+            return result;
+        }
+    }
+
+    // Example usage:
+    class Program
+    {
+        static void Main()
+        {
+            Solution sol = new Solution();
+            string[] words = { "a", "b", "c", "d" };
+            int[] groups = { 1, 0, 1, 1 };
+            Console.WriteLine(string.Join(", ", sol.GetLongestSubsequence(words, groups))); // Output: "a, b, c"
+        }
+    }
+}
