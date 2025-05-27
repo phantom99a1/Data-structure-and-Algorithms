1c47285eeb36520585b742ca7f95bab54c834ace
phantom99a1 <viendanho99a1@gmail.com>
2025-05-27 23:42:34 +0700

    Add the problem Divisible and Non-divisible Sums Difference

---
+++ Divisible and Non-divisible Sums Difference/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Divisible_and_Non_divisible_Sums_Difference
+{
+    using System;
+
+    class Program
+    {
+        static int DifferenceOfSums(int n, int m)
+        {
+            int totalDifference = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                totalDifference += (i % m == 0) ? -i : i;
+            }
+            return totalDifference;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(DifferenceOfSums(10, 3)); // Output: 19
+        }
+    }
+}
