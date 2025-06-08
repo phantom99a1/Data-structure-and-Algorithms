05a26db9edf340082fc2dab28ee2b8ac37bf779a
phantom99a1 <viendanho99a1@gmail.com>
2025-06-08 11:00:32 +0700

    Add the problem Lexicographical Numbers

---
+++ Lexicographical Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lexicographical_Numbers
+{
+    public class Solution
+    {
+        public IList<int> LexicalOrder(int n)
+        {
+            List<int> result = new List<int>();
+            void DFS(int num)
+            {
+                if (num > n) return;
+                result.Add(num);
+                for (int i = 0; i <= 9; i++)
+                {
+                    DFS(num * 10 + i);
+                }
+            }
+            for (int i = 1; i <= 9; i++)
+            {
+                DFS(i);
+            }
+            return result;
+        }
+    }
+}
