78228ccdf848dc47a050f9456efebfb54dd19717
phantom99a1 <viendanho99a1@gmail.com>
2025-06-12 19:30:08 +0700

    Add the problem Maximum Difference Between Adjacent Elements in a Circular Array

---
+++ Maximum Difference Between Adjacent Elements in a Circular Array/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array
+{
+    public class Solution
+    {
+        public int MaxAdjacentDistance(int[] nums)
+        {
+            int maxDiff = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int nextIndex = (i + 1) % nums.Length;
+                maxDiff = Math.Max(maxDiff, Math.Abs(nums[i] - nums[nextIndex]));
+            }
+            return maxDiff;
+        }
+    }
+}
