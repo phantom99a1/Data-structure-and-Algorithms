6d69b251effa1003c1727d72b6a7121046dc8839
phantom99a1 <viendanho99a1@gmail.com>
2025-06-14 09:37:57 +0700

    Add the problem Maximum Difference by Remapping a Digit

---
+++ Maximum Difference by Remapping a Digit/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Maximum_Difference_by_Remapping_a_Digit
+{
+    public class Solution
+    {
+        public int MinMaxDifference(int num)
+        {
+            string str = num.ToString();
+
+            // Max: replace first non-9 digit with 9
+            char maxDigit = str.First(c => c != '9');
+            int max = int.Parse(str.Replace(maxDigit, '9'));
+
+            // Min: replace first digit with 0
+            char minDigit = str[0];
+            int min = int.Parse(str.Replace(minDigit, '0'));
+
+            return max - min;
+        }
+    }
+}
