a436ce60a1bd0673276a79793c5691788b503cfb
phantom99a1 <viendanho99a1@gmail.com>
2025-06-16 17:05:02 +0700

    Add the problem Maximum Difference Between Increasing Elements

---
+++ Maximum Difference Between Increasing Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Maximum_Difference_Between_Increasing_Elements
+{
+    public class Solution
+    {
+        public int MaximumDifference(int[] nums)
+        {
+            int minVal = nums[0];
+            int maxDiff = -1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > minVal)
+                {
+                    maxDiff = Math.Max(maxDiff, nums[i] - minVal);
+                }
+                else
+                {
+                    minVal = nums[i];
+                }
+            }
+
+            return maxDiff;
+        }
+    }
+}
