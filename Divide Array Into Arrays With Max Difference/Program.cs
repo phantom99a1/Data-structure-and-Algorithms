eaeee46cab3975e072d4a798ebce6967a954eb34
phantom99a1 <viendanho99a1@gmail.com>
2025-06-18 22:45:25 +0700

    Add the problem Divide Array Into Arrays With Max Difference

---
+++ Divide Array Into Arrays With Max Difference/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Divide_Array_Into_Arrays_With_Max_Difference
+{
+    internal class Program
+    {
+        public IList<IList<int>> DivideArray(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            var result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i += 3)
+            {
+                if (nums[i + 2] - nums[i] > k) return new List<IList<int>>();
+                result.Add(new List<int> { nums[i], nums[i + 1], nums[i + 2] });
+            }
+
+            return result;
+        }
+    }
+}
