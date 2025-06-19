861592d4c4a29abb9a107794c486b21d0af7466c
phantom99a1 <viendanho99a1@gmail.com>
2025-06-19 18:54:35 +0700

    Add the problem Partition Array Such That Maximum Difference Is K

---
+++ Partition Array Such That Maximum Difference Is K/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Partition_Array_Such_That_Maximum_Difference_Is_K
+{
+    public class Solution
+    {
+        public int PartitionArray(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int count = 1;
+            int start = nums[0];
+
+            foreach (int num in nums)
+            {
+                if (num - start > k)
+                {
+                    count++;
+                    start = num;
+                }
+            }
+
+            return count;
+        }
+    }
+}
