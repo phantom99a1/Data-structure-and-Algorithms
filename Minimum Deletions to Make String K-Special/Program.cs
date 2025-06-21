08271f05865d15441b2dd7ceec198fb73c44ccf0
phantom99a1 <viendanho99a1@gmail.com>
2025-06-21 10:19:19 +0700

    Add the problem Minimum Deletions to Make String K-Special

---
+++ Minimum Deletions to Make String K-Special/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Minimum_Deletions_to_Make_String_K_Special
+{
+    public class Solution
+    {
+        public int MinimumDeletions(string word, int k)
+        {
+            int[] freq = new int[26];
+            foreach (char c in word)
+            {
+                freq[c - 'a']++;
+            }
+
+            List<int> nums = freq.Where(x => x > 0).ToList();
+
+            int f(int v)
+            {
+                int deletions = 0;
+                foreach (int x in nums)
+                {
+                    if (x < v) deletions += x;
+                    else if (x > v + k) deletions += x - v - k;
+                }
+                return deletions;
+            }
+
+            int minDel = word.Length;
+            for (int v = 0; v <= word.Length; v++)
+            {
+                minDel = Math.Min(minDel, f(v));
+            }
+            return minDel;
+        }
+    }
+}
