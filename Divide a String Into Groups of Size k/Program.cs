d914b401d65a29fd2380a7f9be79e9fee8add1a5
phantom99a1 <viendanho99a1@gmail.com>
2025-06-22 10:10:07 +0700

    Add the problem Divide a String Into Groups of Size k

---
+++ Divide a String Into Groups of Size k/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Divide_a_String_Into_Groups_of_Size_k
+{
+    public class Solution
+    {
+        public IList<string> DivideString(string s, int k, char fill)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < s.Length; i += k)
+            {
+                string chunk = s.Substring(i, Math.Min(k, s.Length - i));
+                if (chunk.Length < k)
+                {
+                    chunk = chunk.PadRight(k, fill);
+                }
+                result.Add(chunk);
+            }
+            return result;
+        }
+    }
+}
