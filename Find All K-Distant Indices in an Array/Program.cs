996015918ba8eb55e54ee285cee454b70d51e99f
phantom99a1 <viendanho99a1@gmail.com>
2025-06-24 18:36:57 +0700

    Add the problem Find All K-Distant Indices in an Array

---
+++ Find All K-Distant Indices in an Array/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Find_All_K_Distant_Indices_in_an_Array
+{
+    internal class Program
+    {
+        public IList<int> FindKDistantIndices(int[] nums, int key, int k)
+        {
+            var result = new List<int>();
+            var keyIndices = new List<int>();
+
+            // Collect all indices where nums[i] == key
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == key)
+                {
+                    keyIndices.Add(i);
+                }
+            }
+
+            // Check each index to see if it's within k distance of any key index
+            for (int i = 0; i < nums.Length; i++)
+            {
+                foreach (int j in keyIndices)
+                {
+                    if (Math.Abs(i - j) <= k)
+                    {
+                        result.Add(i);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
