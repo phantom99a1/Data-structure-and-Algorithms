904cdfd624cedc705e51170cbe27b9af09433386
phantom99a1 <viendanho99a1@gmail.com>
2025-06-26 19:11:53 +0700

    Add the problem Longest Binary Subsequence Less Than or Equal to K

---
+++ Longest Binary Subsequence Less Than or Equal to K/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Longest_Binary_Subsequence_Less_Than_or_Equal_to_K
+{
+    public class Solution
+    {
+        public int LongestSubsequence(string s, int k)
+        {
+            int ans = 0;
+            long val = 0, power = 1;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == '0')
+                {
+                    ans++;
+                }
+                else
+                {
+                    if (power <= k && val + power <= k)
+                    {
+                        val += power;
+                        ans++;
+                    }
+                }
+                if (power <= k) power *= 2;
+            }
+            return ans;
+        }
+    }
+}
