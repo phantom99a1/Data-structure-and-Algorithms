3007bf7469f2b160e9bfa6a4a42a525988c3db5c
phantom99a1 <viendanho99a1@gmail.com>
2025-06-28 10:12:09 +0700

    Add the problem Find Subsequence of Length K With the Largest Sum

---
+++ Find Subsequence of Length K With the Largest Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Find_Subsequence_of_Length_K_With_the_Largest_Sum
+{
+    public class Solution
+    {
+        public int[] MaxSubsequence(int[] nums, int k)
+        {
+            int len = nums.Length, index = 0;
+            //to keep the order of original array
+            var copy = new int[len];
+            Array.Copy(nums, copy, len);
+            Array.Sort(nums);
+            var excludes = nums[0..(len - k)].ToList();
+            var res = new int[k];
+            foreach (var c in copy)
+            {
+                if (excludes.Contains(c))
+                {
+                    excludes.Remove(c);
+                }
+                else
+                {
+                    res[index++] = c;
+                }
+            }
+            return res;
+        }
+    }
+}
