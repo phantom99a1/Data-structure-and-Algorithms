6aa96cfe88e4007f17587e4078a7f59b632a4486
phantom99a1 <viendanho99a1@gmail.com>
2025-06-29 15:19:52 +0700

    Add the problem Number of Subsequences That Satisfy the Given Sum Condition

---
+++ Number of Subsequences That Satisfy the Given Sum Condition/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition
+{
+    public class Solution
+    {
+        public int NumSubseq(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int mod = 1_000_000_007;
+            int[] pow = new int[nums.Length];
+            pow[0] = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                pow[i] = (pow[i - 1] * 2) % mod;
+            }
+
+            int left = 0, right = nums.Length - 1, result = 0;
+
+            while (left <= right)
+            {
+                if (nums[left] + nums[right] <= target)
+                {
+                    result = (result + pow[right - left]) % mod;
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
