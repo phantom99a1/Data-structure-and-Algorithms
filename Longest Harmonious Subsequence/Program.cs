e25ab8fcc63e689be70a2d8073dcfd0b33a432b4
phantom99a1 <viendanho99a1@gmail.com>
2025-06-30 19:22:56 +0700

    Add the problem Longest Harmonious Subsequence

---
+++ Longest Harmonious Subsequence/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Longest_Harmonious_Subsequence
+{
+    public class Solution
+    {
+        public int FindLHS(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            int maxLen = 0;
+
+            foreach (int num in nums)
+            {
+                if (!dict.ContainsKey(num))
+                    dict[num] = 0;
+                dict[num]++;
+            }
+
+            foreach (var kvp in dict)
+            {
+                if (dict.ContainsKey(kvp.Key + 1))
+                {
+                    maxLen = Math.Max(maxLen, kvp.Value + dict[kvp.Key + 1]);
+                }
+            }
+
+            return maxLen;
+        }
+    }
+}
