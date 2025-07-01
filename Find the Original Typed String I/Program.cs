d8359fc12fc4483e856c74fbbd53383d80d13ce9
phantom99a1 <viendanho99a1@gmail.com>
2025-07-01 18:26:33 +0700

    Add the problem Find the Original Typed String I

---
+++ Find the Original Typed String I/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Find_the_Original_Typed_String_I
+{
+    public class Solution
+    {
+        public int PossibleStringCount(string word)
+        {
+            int count = 1;
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] == word[i - 1])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
