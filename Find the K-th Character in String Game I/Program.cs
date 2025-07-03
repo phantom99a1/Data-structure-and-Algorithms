4698d41af8707e0202d9e8c70dbcb172356d76ff
phantom99a1 <viendanho99a1@gmail.com>
2025-07-03 18:32:54 +0700

    Add the problem Find the K-th Character in String Game I

---
+++ Find the K-th Character in String Game I/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Find_the_K_th_Character_in_String_Game_I
+{
+    public class Solution
+    {
+        public char FindKthCharacter(int k)
+        {
+            List<int> word = new List<int> { 0 }; // 'a' is 0
+            while (word.Count < k)
+            {
+                int size = word.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    word.Add((word[i] + 1) % 26);
+                }
+            }
+            return (char)('a' + word[k - 1]);
+        }
+    }
+}
