d6c4dbdd26e68b69bc7a07fe2d593a078696fbcc
phantom99a1 <viendanho99a1@gmail.com>
2025-07-05 11:24:50 +0700

    Add the problem Find Lucky Integer in an Array

---
+++ Find Lucky Integer in an Array/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Find_Lucky_Integer_in_an_Array
+{
+    internal class Program
+    {
+        public int FindLucky(int[] arr)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (int num in arr)
+            {
+                if (dict.ContainsKey(num))
+                    dict[num]++;
+                else
+                    dict[num] = 1;
+            }
+
+            int result = -1;
+            foreach (var kvp in dict)
+            {
+                if (kvp.Key == kvp.Value)
+                    result = Math.Max(result, kvp.Key);
+            }
+
+            return result;
+        }
+    }
+}
