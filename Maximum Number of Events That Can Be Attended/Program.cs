1c73f9778cd26b26f09d39c882ef36ca2da45374
phantom99a1 <viendanho99a1@gmail.com>
2025-07-07 18:39:57 +0700

    Add the problem Maximum Number of Events That Can Be Attended

---
+++ Maximum Number of Events That Can Be Attended/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Maximum_Number_of_Events_That_Can_Be_Attended
+{
+    public class Solution
+    {
+        public int MaxEvents(int[][] events)
+        {
+            Array.Sort(events, (a, b) => a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+
+            bool[] seen = new bool[100001];
+            int count = 0;
+            int lastFound = 0;
+            for (int i = 0; i < events.Length; i++)
+            {
+                int end = events[i][1];
+                int start = (i > 0 && events[i - 1][0] == events[i][0]) ? lastFound : events[i][0];
+
+                for (int j = start; j <= end; j++)
+                {
+                    if (!seen[j])
+                    {
+                        seen[j] = true;
+                        lastFound = j;
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
