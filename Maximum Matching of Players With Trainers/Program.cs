67bebc8a888e2caf969e5dafb6f50e4a9128597a
phantom99a1 <viendanho99a1@gmail.com>
2025-07-13 11:20:08 +0700

    Add the problem Maximum Matching of Players With Trainers

---
+++ Maximum Matching of Players With Trainers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Maximum_Matching_of_Players_With_Trainers
+{
+    public class Solution
+    {
+        public int MatchPlayersAndTrainers(int[] players, int[] trainers)
+        {
+            Array.Sort(players);
+            Array.Sort(trainers);
+
+            int i = 0, j = 0, matches = 0;
+
+            while (i < players.Length && j < trainers.Length)
+            {
+                if (players[i] <= trainers[j])
+                {
+                    matches++;
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return matches;
+        }
+    }
+}
