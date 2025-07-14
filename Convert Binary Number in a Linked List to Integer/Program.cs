845bffa22c8b4496b710532a0141e3c832a7768e
phantom99a1 <viendanho99a1@gmail.com>
2025-07-14 20:13:31 +0700

    Add the problem Convert Binary Number in a Linked List to Integer

---
+++ Convert Binary Number in a Linked List to Integer/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Convert_Binary_Number_in_a_Linked_List_to_Integer
+{
+
+    public class ListNode {
+         public int val;
+         public ListNode next;
+         public ListNode(int val=0, ListNode next=null) {
+             this.val = val;
+             this.next = next;
+         }
+     }
+
+    public class Solution
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int num = 0;
+            while (head != null)
+            {
+                num = (num << 1) | head.val;
+                head = head.next;
+            }
+            return num;
+        }
+    }
+}
