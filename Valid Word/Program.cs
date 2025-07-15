45f74f8f38440484bbc0af4a2c55c3e5e15f708c
phantom99a1 <viendanho99a1@gmail.com>
2025-07-15 23:40:01 +0700

    Add the problem Valid Word

---
+++ Valid Word/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Valid_Word
+{
+    public class Solution
+    {
+        public bool IsValid(string word)
+        {
+            if (word.Length < 3)
+            {
+                return false;
+            }
+            bool hasVowel = false;
+            bool hasConsonant = false;
+            foreach (char c in word)
+            {
+                if (char.IsLetter(c))
+                {
+                    char ch = char.ToLower(c);
+                    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||
+                        ch == 'u')
+                    {
+                        hasVowel = true;
+                    }
+                    else
+                    {
+                        hasConsonant = true;
+                    }
+                }
+                else if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return hasVowel && hasConsonant;
+        }
+    }
+}
