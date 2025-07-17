eb5eba93b427d2b20c2202ef48cb25085421700d
phantom99a1 <viendanho99a1@gmail.com>
2025-07-17 18:25:50 +0700

    Add the problem Find the Maximum Length of Valid Subsequence II

---
+++ Find the Maximum Length of Valid Subsequence II/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_the_Maximum_Length_of_Valid_Subsequence_II
+{
+    public class Solution
+    {
+        public int MaximumLength(int[] nums, int k)
+        {
+            int[,] dp = new int[k, k];
+            int maxLen = 0;
+
+            foreach (int num in nums)
+            {
+                int mod = num % k;
+                for (int y = 0; y < k; ++y)
+                {
+                    dp[mod, y] = dp[y, mod] + 1;
+                    maxLen = Math.Max(maxLen, dp[mod, y]);
+                }
+            }
+
+            return maxLen;
+        }
+    }
+}
