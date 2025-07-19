37ab545edb6ca66e4e9fe0430f5d4324dbe7b0dc
phantom99a1 <viendanho99a1@gmail.com>
2025-07-19 10:49:20 +0700

    Add the problem Remove Sub-Folders from the Filesystem

---
+++ Remove Sub-Folders from the Filesystem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Remove_Sub_Folders_from_the_Filesystem
+{
+    public class Solution
+    {
+        public IList<string> RemoveSubfolders(string[] folder)
+        {
+            Array.Sort(folder);
+            List<string> result = new List<string> { folder[0] };
+
+            for (int i = 1; i < folder.Length; i++)
+            {
+                string prev = result[result.Count - 1];
+                if (!folder[i].StartsWith(prev + "/"))
+                {
+                    result.Add(folder[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
