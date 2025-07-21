c56e05eae5cb35dd2345d3b09bc99e9cd5e3d911
phantom99a1 <viendanho99a1@gmail.com>
2025-07-21 18:31:29 +0700

    Add the problem Delete Characters to Make Fancy String

---
+++ Delete Characters to Make Fancy String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Delete_Characters_to_Make_Fancy_String
+{
+    public class Solution
+    {
+        public string MakeFancyString(string s)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (result.Length >= 2 &&
+                    result[result.Length - 1] == s[i] &&
+                    result[result.Length - 2] == s[i])
+                {
+                    continue; // Skip the third identical character
+                }
+                result.Append(s[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
