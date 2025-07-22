895d791503ba5c09e78f8cd59b8fafe22a2afd11
phantom99a1 <viendanho99a1@gmail.com>
2025-07-22 21:46:25 +0700

    add the problem Maximum Erasure Value

---
+++ Maximum Erasure Value/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Maximum_Erasure_Value
+{
+    internal class Program
+    {
+        public int MaximumUniqueSubarray(int[] nums)
+        {
+            var seen = new HashSet<int>();
+            int left = 0, sum = 0, maxSum = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                while (seen.Contains(nums[right]))
+                {
+                    seen.Remove(nums[left]);
+                    sum -= nums[left];
+                    left++;
+                }
+                seen.Add(nums[right]);
+                sum += nums[right];
+                maxSum = Math.Max(maxSum, sum);
+            }
+
+            return maxSum;
+        }
+    }
+}
