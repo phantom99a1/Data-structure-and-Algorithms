8c9604a3574af49895c07349b4d44f58e6946714
phantom99a1 <viendanho99a1@gmail.com>
2025-07-23 19:06:39 +0700

    Add the problem Maximum Score From Removing Substrings

---
+++ Maximum Score From Removing Substrings/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Maximum_Score_From_Removing_Substrings
+{
+    public class Solution
+    {
+        public int MaximumGain(string s, int x, int y)
+        {
+            int score = 0;
+            Stack<char> stack = new Stack<char>();
+
+            string Remove(string str, char a, char b, int points)
+            {
+                foreach (char ch in str)
+                {
+                    if (stack.Count > 0 && stack.Peek() == a && ch == b)
+                    {
+                        stack.Pop();
+                        score += points;
+                    }
+                    else
+                    {
+                        stack.Push(ch);
+                    }
+                }
+                var result = new string(stack.Reverse().ToArray());
+                stack.Clear();
+                return result;
+            }
+
+            if (x >= y)
+            {
+                s = Remove(s, 'a', 'b', x);
+                Remove(s, 'b', 'a', y);
+            }
+            else
+            {
+                s = Remove(s, 'b', 'a', y);
+                Remove(s, 'a', 'b', x);
+            }
+
+            return score;
+        }
+    }
+}
