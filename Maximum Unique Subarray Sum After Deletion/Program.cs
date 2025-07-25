fddca0ad2669c90c905744a6b6048f20ae50e3e6
phantom99a1 <viendanho99a1@gmail.com>
2025-07-25 17:55:42 +0700

    Add the problem Maximum Unique Subarray Sum After Deletion

---
+++ Maximum Unique Subarray Sum After Deletion/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Maximum_Unique_Subarray_Sum_After_Deletion
+{
+    public class Solution
+    {
+        public int MaxSum(int[] nums)
+        {
+            int maxVal = nums.Max();
+            if (maxVal <= 0) return maxVal;
+
+            HashSet<int> seen = new HashSet<int>();
+            int sum = 0;
+
+            foreach (int num in nums)
+            {
+                if (num > 0 && !seen.Contains(num))
+                {
+                    sum += num;
+                    seen.Add(num);
+                }
+            }
+
+            return sum;
+        }
+    }
+}
