1c514e93489336f2011ce5c225d824fb335bf30f
phantom99a1 <viendanho99a1@gmail.com>
2025-07-27 13:58:28 +0700

    Add the problem Count Hills and Valleys in an Array

---
+++ Count Hills and Valleys in an Array/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Count_Hills_and_Valleys_in_an_Array
+{
+    public class Solution
+    {
+        public int CountHillValley(int[] nums)
+        {
+            if (nums.Length < 3) return 0;
+            int l = 0;
+            int i = 1;
+            int c = 0;
+            for (int r = 2; r < nums.Length; r++)
+            {
+                if (nums[l] == nums[i])
+                {
+                    i++;
+                    l++;
+                }
+                else if (nums[r] == nums[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    if ((nums[l] < nums[i] && nums[i] > nums[r]) || (nums[l] > nums[i] && nums[i] < nums[r]))
+                    {
+                        c++;
+                    }
+
+                    l = r - 1;
+                    i = r;
+                }
+            }
+            return c;
+
+        }
+    }
+}
