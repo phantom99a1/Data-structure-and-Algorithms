fd2464c8ed10430055a9bcdb8d01aec06f950539
phantom99a1 <viendanho99a1@gmail.com>
2025-07-28 19:19:39 +0700

    Add the problem Count Number of Maximum Bitwise-OR Subsets

---
+++ Count Number of Maximum Bitwise-OR Subsets/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Count_Number_of_Maximum_Bitwise_OR_Subsets
+{
+    public class Solution
+    {
+        public int CountMaxOrSubsets(int[] nums)
+        {
+            int maxOr = 0, count = 0;
+
+            void Backtrack(int index, int currentOr)
+            {
+                if (index == nums.Length)
+                {
+                    if (currentOr > maxOr)
+                    {
+                        maxOr = currentOr;
+                        count = 1;
+                    }
+                    else if (currentOr == maxOr)
+                    {
+                        count++;
+                    }
+                    return;
+                }
+                // Include nums[index]
+                Backtrack(index + 1, currentOr | nums[index]);
+                // Exclude nums[index]
+                Backtrack(index + 1, currentOr);
+            }
+
+            Backtrack(0, 0);
+            return count;
+        }
+    }
+}
