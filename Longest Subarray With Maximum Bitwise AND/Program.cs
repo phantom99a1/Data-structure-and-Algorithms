eede30a5918f883f0c1e846098626f3292ca7171
phantom99a1 <viendanho99a1@gmail.com>
2025-07-30 18:27:24 +0700

    Add the problem Longest Subarray With Maximum Bitwise AND

---
+++ Longest Subarray With Maximum Bitwise AND/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Longest_Subarray_With_Maximum_Bitwise_AND
+{
+    public class Solution
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            int maxVal = nums.Max();
+            int maxLen = 0, currentLen = 0;
+
+            foreach (int num in nums)
+            {
+                if (num == maxVal)
+                {
+                    currentLen++;
+                    maxLen = Math.Max(maxLen, currentLen);
+                }
+                else
+                {
+                    currentLen = 0;
+                }
+            }
+
+            return maxLen;
+        }
+    }
+}
