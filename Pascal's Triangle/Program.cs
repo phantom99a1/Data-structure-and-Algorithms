9eed7124b211e8e86cf797b626b7c7f9d4e99ff2
phantom99a1 <viendanho99a1@gmail.com>
2025-08-01 18:32:02 +0700

    Add the problem Pascal's Triangle

---
+++ Pascal's Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Pascal_s_Triangle
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var triangle = new List<IList<int>>();
+
+            if (numRows >= 1)
+            {
+                triangle.Add(new List<int> { 1 });
+            }
+
+            for (int row = 1; row < numRows; row++)
+            {
+                var prevRow = triangle[row - 1];
+                var currRow = new List<int>
+                {
+                    1 // First element is always 1
+                };
+
+                for (int j = 1; j < row; j++)
+                {
+                    int val = prevRow[j - 1] + prevRow[j];
+                    currRow.Add(val);
+                }
+
+                currRow.Add(1); // Last element is always 1
+                triangle.Add(currRow);
+            }
+
+            return triangle;
+        }
+    }
+}
