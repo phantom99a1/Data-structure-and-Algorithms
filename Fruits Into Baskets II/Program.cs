d306c39bdc04d2751d5cca7b52c2b0b1060972ad
phantom99a1 <viendanho99a1@gmail.com>
2025-08-05 21:18:18 +0700

    Add the problem Fruits Into Baskets II

---
+++ Fruits Into Baskets II/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fruits_Into_Baskets_II
+{
+    public class Solution
+    {
+        public int NumOfUnplacedFruits(int[] fruits, int[] baskets)
+        {
+            int ans = 0;
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                bool check = false;
+                for (int j = 0; j < baskets.Length; j++)
+                {
+                    if (fruits[i] <= baskets[j])
+                    {
+                        check = true;
+                        baskets[j] = 0;
+                        break;
+                    }
+                }
+                if (check == false)
+                {
+                    ans += 1;
+                }
+            }
+            return ans;
+
+        }
+    }
+}
