6a580ed56ed7a7018a52d39b49a57fa199fa1263
phantom99a1 <viendanho99a1@gmail.com>
2025-08-09 14:43:21 +0700

    Add the problem Power of Two

---
+++ Power of Two/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Power_of_Two
+{
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0) return false;
+            return (n & (n - 1)) == 0;
+        }
+    }
+}
