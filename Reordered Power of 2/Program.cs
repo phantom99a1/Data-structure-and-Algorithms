3341bfd19c04ac6a5203b395f7ef84160a57ca26
phantom99a1 <viendanho99a1@gmail.com>
2025-08-10 10:43:17 +0700

    Add the problem Reordered Power of 2

---
+++ Reordered Power of 2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Reordered_Power_of_2
+{
+    public class Solution
+    {
+        public bool ReorderedPowerOf2(int n)
+        {
+            string CountDigits(int num)
+            {
+                char[] digits = num.ToString().ToCharArray();
+                Array.Sort(digits);
+                return new string(digits);
+            }
+
+            string target = CountDigits(n);
+            for (int i = 0; i < 31; i++)
+            {
+                int power = 1 << i;
+                if (CountDigits(power) == target)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
