55e0ef7b20181085f611201adcb62cea23678bd1
phantom99a1 <viendanho99a1@gmail.com>
2025-08-13 20:27:32 +0700

    Add the problem Power of Three

---
+++ Power of Three/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Power_of_Three
+{
+    public class Solution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n < 1) return false;
+            while (n % 3 == 0)
+            {
+                n /= 3;
+            }
+            return n == 1;
+        }
+    }
+}
