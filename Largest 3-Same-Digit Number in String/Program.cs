239b0f5d3d16d0ae3cb0da796fa5e3bbdcf9fbe7
phantom99a1 <viendanho99a1@gmail.com>
2025-08-14 18:41:19 +0700

    Add the problem Largest 3-Same-Digit Number in String

---
+++ Largest 3-Same-Digit Number in String/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Largest_3_Same_Digit_Number_in_String
+{
+    public class Solution
+    {
+        public string LargestGoodInteger(string num)
+        {
+            for (char c = '9'; c >= '0'; c--)
+            {
+                string triplet = new string(c, 3);
+                if (num.Contains(triplet))
+                {
+                    return triplet;
+                }
+            }
+            return "";
+        }
+    }
+}
