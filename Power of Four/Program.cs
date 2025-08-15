56570ba4b0a9653f67c488a4db6afeeaa45ec661
phantom99a1 <viendanho99a1@gmail.com>
2025-08-15 23:12:12 +0700

    Add the problem Power of Four

---
+++ Power of Four/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Power_of_Four
+{
+    public class Solution
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+        }
+    }
+}
