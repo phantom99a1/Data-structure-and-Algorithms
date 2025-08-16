ef727588bba89d722e93a98079646e4529689bb4
phantom99a1 <viendanho99a1@gmail.com>
2025-08-17 00:36:26 +0700

    Add the problem Maximum 69 Number

---
+++ Maximum 69 Number/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Maximum_69_Number
+{
+    public class Solution
+    {
+        public int Maximum69Number(int num)
+        {
+            char[] digits = num.ToString().ToCharArray();
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] == '6')
+                {
+                    digits[i] = '9';
+                    break; // Only one change allowed
+                }
+            }
+
+            return int.Parse(new string(digits));
+        }
+    }
+}
