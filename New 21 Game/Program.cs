b254caa7fc9786d4c48fae0915d99349ddc93896
phantom99a1 <viendanho99a1@gmail.com>
2025-08-17 10:18:08 +0700

    Add the problem New 21 Game

---
+++ New 21 Game/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace New_21_Game
+{
+    public class Solution
+    {
+        public double New21Game(int N, int K, int W)
+        {
+            if (K == 0 || N >= K + W) return 1.0;
+
+            double[] dp = new double[N + 1];
+            dp[0] = 1.0;
+            double Wsum = 1.0, result = 0.0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                dp[i] = Wsum / W;
+                if (i < K)
+                {
+                    Wsum += dp[i];
+                }
+                else
+                {
+                    result += dp[i];
+                }
+                if (i - W >= 0)
+                {
+                    Wsum -= dp[i - W];
+                }
+            }
+
+            return result;
+        }
+    }
+}
