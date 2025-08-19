46e512a9a79f6c5108b9c0db9ec330a0bd6150f2
phantom99a1 <viendanho99a1@gmail.com>
2025-08-19 12:06:52 +0700

    Add the problem Number of Zero-Filled Subarrays

---
+++ Number of Zero-Filled Subarrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Number_of_Zero_Filled_Subarrays
+{
+    public class Solution
+    {
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            long count = 0, result = 0;
+            foreach (int num in nums)
+            {
+                if (num == 0)
+                {
+                    count++;
+                    result += count;
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
