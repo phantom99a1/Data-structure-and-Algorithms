ab9828ac4a86218eb0daf586f1b6b0f86b130ef8
phantom99a1 <viendanho99a1@gmail.com>
2025-08-22 21:51:33 +0700

    Add the problem Find the Minimum Area to Cover All Ones I

---
+++ Find the Minimum Area to Cover All Ones I/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Find_the_Minimum_Area_to_Cover_All_Ones_I
+{
+    public class Solution
+    {
+        public int MinimumArea(int[][] grid)
+        {
+            int m = grid.Length, n = grid[0].Length;
+            int x1 = m, y1 = n, x2 = 0, y2 = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        x1 = Math.Min(x1, i);
+                        y1 = Math.Min(y1, j);
+                        x2 = Math.Max(x2, i);
+                        y2 = Math.Max(y2, j);
+                    }
+                }
+            }
+
+            return (x2 - x1 + 1) * (y2 - y1 + 1);
+        }
+    }
+}
