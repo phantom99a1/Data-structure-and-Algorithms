ee97e8967d4f13f893662559e79f8ba52f36bafa
phantom99a1 <viendanho99a1@gmail.com>
2025-08-24 13:00:01 +0700

    Add the problem Longest Subarray of 1's After Deleting One Element

---
+++ Longest Subarray of 1's After Deleting One Element/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Longest_Subarray_of_1_s_After_Deleting_One_Element
+{
+    public class Solution
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            int left = 0, right = 0, zeroCount = 0, maxLen = 0;
+
+            while (right < nums.Length)
+            {
+                if (nums[right] == 0) zeroCount++;
+
+                while (zeroCount > 1)
+                {
+                    if (nums[left] == 0) zeroCount--;
+                    left++;
+                }
+
+                maxLen = Math.Max(maxLen, right - left);
+                right++;
+            }
+
+            return maxLen;
+        }
+    }
+}
