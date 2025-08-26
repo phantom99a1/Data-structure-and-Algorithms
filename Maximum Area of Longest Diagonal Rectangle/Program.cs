a9591df2f55eb3a65cfc50692f57fea55cc2d38e
phantom99a1 <viendanho99a1@gmail.com>
2025-08-26 18:12:59 +0700

    Add the problem Maximum Area of Longest Diagonal Rectangle

---
+++ Maximum Area of Longest Diagonal Rectangle/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Maximum_Area_of_Longest_Diagonal_Rectangle
+{
+    public class Solution
+    {
+        public int AreaOfMaxDiagonal(int[][] dimensions)
+        {
+            int maxDiagonalSq = 0;
+            int maxArea = 0;
+
+            foreach (var rect in dimensions)
+            {
+                int length = rect[0], width = rect[1];
+                int diagonalSq = length * length + width * width;
+                int area = length * width;
+
+                if (diagonalSq > maxDiagonalSq)
+                {
+                    maxDiagonalSq = diagonalSq;
+                    maxArea = area;
+                }
+                else if (diagonalSq == maxDiagonalSq)
+                {
+                    maxArea = Math.Max(maxArea, area);
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
