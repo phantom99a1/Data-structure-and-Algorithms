a93959dbd92c2c3ae3e1907d3cf6086bf1295dca
phantom99a1 <viendanho99a1@gmail.com>
2025-09-04 22:07:20 +0700

    Add the problem Find Closest Person

---
+++ Find Closest Person/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Find_Closest_Person
+{
+    public class Solution
+    {
+        public int FindClosest(int x, int y, int z)
+        {
+            int d1 = Math.Abs(z - x);
+            int d2 = Math.Abs(z - y);
+
+            if (d1 < d2) return 1;
+            else if (d1 > d2) return 2;
+            else return 0;
+        }
+    }
+}
