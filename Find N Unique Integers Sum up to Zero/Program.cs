6ae7bb3162f1d24ead044ecd1a8991b4cd9b725b
phantom99a1 <viendanho99a1@gmail.com>
2025-09-07 11:44:26 +0700

    Add the problem Find N Unique Integers Sum up to Zero

---
+++ Find N Unique Integers Sum up to Zero/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Find_N_Unique_Integers_Sum_up_to_Zero
+{
+    internal class Program
+    {
+        public int[] SumZero(int n)
+        {
+            List<int> result = new List<int>();
+            for (int i = 1; i <= n / 2; i++)
+            {
+                result.Add(i);
+                result.Add(-i);
+            }
+            if (n % 2 != 0)
+            {
+                result.Add(0);
+            }
+            return result.ToArray();
+        }
+    }
+}
