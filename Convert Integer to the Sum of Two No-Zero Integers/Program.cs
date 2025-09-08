7cb199c63b0dd7ec6d696f89f65cfe936b810900
phantom99a1 <viendanho99a1@gmail.com>
2025-09-08 19:16:46 +0700

    Add the problem Convert Integer to the Sum of Two No-Zero Integers

---
+++ Convert Integer to the Sum of Two No-Zero Integers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Convert_Integer_to_the_Sum_of_Two_No_Zero_Integers
+{
+    public class Solution
+    {
+        public int[] GetNoZeroIntegers(int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                int j = n - i;
+                if (!HasZero(i) && !HasZero(j))
+                {
+                    return new int[] { i, j };
+                }
+            }
+            return null;
+        }
+
+        private bool HasZero(int num)
+        {
+            while (num > 0)
+            {
+                if (num % 10 == 0) return true;
+                num /= 10;
+            }
+            return false;
+        }
+    }
+}
