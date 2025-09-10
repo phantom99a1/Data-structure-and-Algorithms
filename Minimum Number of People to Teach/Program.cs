3fcd711e2a04a4e47a4b92f7f5bb335f10dfe801
phantom99a1 <viendanho99a1@gmail.com>
2025-09-10 22:37:09 +0700

    Add the problem Minimum Number of People to Teach

---
+++ Minimum Number of People to Teach/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Minimum_Number_of_People_to_Teach
+{
+    internal class Program
+    {
+        public int MinimumTeachings(int n, IList<IList<int>> languages, IList<IList<int>> friendships)
+        {
+            var knows = new Dictionary<int, HashSet<int>>();
+            for (int i = 0; i < languages.Count; i++)
+            {
+                knows[i + 1] = new HashSet<int>(languages[i]);
+            }
+
+            var toTeach = new HashSet<int>();
+            foreach (var pair in friendships)
+            {
+                int u = pair[0], v = pair[1];
+                var uLangs = knows[u];
+                var vLangs = knows[v];
+                if (!uLangs.Intersect(vLangs).Any())
+                {
+                    toTeach.Add(u);
+                    toTeach.Add(v);
+                }
+            }
+
+            int minTeach = int.MaxValue;
+            for (int lang = 1; lang <= n; lang++)
+            {
+                int count = 0;
+                foreach (int user in toTeach)
+                {
+                    if (!knows[user].Contains(lang)) count++;
+                }
+                minTeach = Math.Min(minTeach, count);
+            }
+
+            return minTeach;
+        }
+    }
+}
