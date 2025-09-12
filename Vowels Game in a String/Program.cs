cc66a3bd37406ae20cc01d293755a7fdf1099ed9
phantom99a1 <viendanho99a1@gmail.com>
2025-09-12 22:56:34 +0700

    Add the problem Vowels Game in a String

---
+++ Vowels Game in a String/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Vowels_Game_in_a_String
+{
+    public class Solution
+    {
+        public bool DoesAliceWin(string s)
+        {
+            foreach (char c in s)
+            {
+                if ("aeiou".Contains(c))
+                {
+                    return true; // Alice wins
+                }
+            }
+            return false; // Bob wins
+        }
+    }
+}
