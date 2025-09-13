1df36d43f7cb39b54874de4379106594a3afb5e1
phantom99a1 <viendanho99a1@gmail.com>
2025-09-13 14:18:44 +0700

    Add the problem Find Most Frequent Vowel and Consonant

---
+++ Find Most Frequent Vowel and Consonant/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Find_Most_Frequent_Vowel_and_Consonant
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public int MaxFreqSum(string s)
+        {
+            Dictionary<char, int> freq = new Dictionary<char, int>();
+            int con = 0, vow = 0;
+            foreach (char c in s)
+            {
+                if (!freq.ContainsKey(c)) freq[c] = 0;
+                freq[c]++;
+            }
+            foreach (var kv in freq)
+            {
+                if ("aeiou".Contains(kv.Key))
+                    vow = Math.Max(vow, kv.Value);
+                else
+                    con = Math.Max(con, kv.Value);
+            }
+            return con + vow;
+        }
+    }
+}
