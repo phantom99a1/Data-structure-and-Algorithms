bc2bcf4983c95f5310b244462d4267b0d170c062
phantom99a1 <viendanho99a1@gmail.com>
2025-09-15 10:33:07 +0700

    Add the problem Maximum Number of Words You Can Type

---
+++ Maximum Number of Words You Can Type/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Maximum_Number_of_Words_You_Can_Type
+{
+    public class Solution
+    {
+        public int CanBeTypedWords(string text, string brokenLetters)
+        {
+            var brokenSet = new HashSet<char>(brokenLetters);
+            var words = text.Split(' ');
+            int count = 0;
+
+            foreach (var word in words)
+            {
+                bool canType = true;
+                foreach (var c in word)
+                {
+                    if (brokenSet.Contains(c))
+                    {
+                        canType = false;
+                        break;
+                    }
+                }
+                if (canType) count++;
+            }
+
+            return count;
+        }
+    }
+}
