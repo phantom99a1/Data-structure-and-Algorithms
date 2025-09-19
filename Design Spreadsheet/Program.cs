0ecceba6e51915a779ec92ef4108350382d9ab98
phantom99a1 <viendanho99a1@gmail.com>
2025-09-19 19:05:43 +0700

    Add the problem Design Spreadsheet

---
+++ Design Spreadsheet/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Design_Spreadsheet
+{
+    public class Spreadsheet
+    {
+        private int rows;
+        private Dictionary<string, int> cells;
+
+        public Spreadsheet(int rows)
+        {
+            this.rows = rows;
+            this.cells = new Dictionary<string, int>();
+        }
+
+        public void SetCell(string cell, int value)
+        {
+            cells[cell] = value;
+        }
+
+        public void ResetCell(string cell)
+        {
+            cells.Remove(cell);
+        }
+
+        public int GetValue(string formula)
+        {
+            string[] parts = formula.Substring(1).Split('+');
+            int GetVal(string s) => int.TryParse(s, out int v) ? v : cells.GetValueOrDefault(s, 0);
+            return GetVal(parts[0]) + GetVal(parts[1]);
+        }
+    }
+}
