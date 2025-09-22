f40051a6b68ba5f8f177298754c37ce11d6cd5ab
phantom99a1 <viendanho99a1@gmail.com>
2025-09-22 22:49:29 +0700

    Add the problem Count Elements With Maximum Frequency

---
+++ Count Elements With Maximum Frequency/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Count_Elements_With_Maximum_Frequency
+{
+    internal class Program
+    {
+        public int MaxFrequencyElements(int[] nums)
+        {
+            int[] freq = new int[101];
+            int maxFreq = 0;
+            int result = 0;
+
+            foreach (int num in nums)
+            {
+                freq[num]++;
+                maxFreq = Math.Max(maxFreq, freq[num]);
+            }
+
+            foreach (int count in freq)
+            {
+                if (count == maxFreq)
+                {
+                    result += count;
+                }
+            }
+
+            return result;
+        }
+    }
+}
