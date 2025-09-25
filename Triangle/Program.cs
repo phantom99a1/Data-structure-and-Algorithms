0c8f459ebcbce47cb86de9351dd5318596299434
phantom99a1 <viendanho99a1@gmail.com>
2025-09-25 19:00:30 +0700

    Add the problem Triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Triangle
+{
+    public class Solution
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            for (int row = triangle.Count - 2; row >= 0; row--)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    int belowLeft = triangle[row + 1][col];
+                    int belowRight = triangle[row + 1][col + 1];
+                    triangle[row][col] += Math.Min(belowLeft, belowRight);
+                }
+            }
+            return triangle[0][0];
+        }
+    }
+}
