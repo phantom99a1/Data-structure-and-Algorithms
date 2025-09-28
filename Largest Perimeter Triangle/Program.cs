6fca6fbbf4f207fbde7b899a025fd79aff95a537
phantom99a1 <viendanho99a1@gmail.com>
2025-09-28 10:10:07 +0700

    Add the problem Largest Perimeter Triangle

---
+++ Largest Perimeter Triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Largest_Perimeter_Triangle
+{
+    public class Solution
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            Array.Reverse(nums); // Sort in descending order
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (nums[i] < nums[i + 1] + nums[i + 2])
+                {
+                    return nums[i] + nums[i + 1] + nums[i + 2];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
