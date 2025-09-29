a23aa6a04af77d20022cf417a30a875071232e8f
phantom99a1 <viendanho99a1@gmail.com>
2025-09-29 20:06:50 +0700

    Add the problem Minimum Score Triangulation of Polygon

---
+++ Minimum Score Triangulation of Polygon/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Minimum_Score_Triangulation_of_Polygon
+{
+    public class Solution
+    {
+        public int MinScoreTriangulation(int[] values)
+        {
+            int n = values.Length;
+            int[,] dp = new int[n, n];
+
+            for (int length = 2; length < n; length++)
+            {
+                for (int i = 0; i + length < n; i++)
+                {
+                    int j = i + length;
+                    dp[i, j] = int.MaxValue;
+                    for (int k = i + 1; k < j; k++)
+                    {
+                        int score = values[i] * values[j] * values[k] + dp[i, k] + dp[k, j];
+                        dp[i, j] = Math.Min(dp[i, j], score);
+                    }
+                }
+            }
+
+            return dp[0, n - 1];
+        }
+    }
+}
