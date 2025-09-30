ed446a12de6220d08482dae362fe5f490ef9f915
phantom99a1 <viendanho99a1@gmail.com>
2025-09-30 18:55:37 +0700

    Add the problem Find Triangular Sum of an Array

---
+++ Find Triangular Sum of an Array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Find_Triangular_Sum_of_an_Array
+{
+    public class Solution
+    {
+        public int TriangularSum(int[] nums)
+        {
+            while (nums.Length > 1)
+            {
+                int[] newNums = new int[nums.Length - 1];
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    newNums[i] = (nums[i] + nums[i + 1]) % 10;
+                }
+                nums = newNums;
+            }
+            return nums[0];
+        }
+    }
+}
