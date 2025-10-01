ee85c00cdfcb18016568370122e638d05795d79e
phantom99a1 <viendanho99a1@gmail.com>
2025-10-01 18:25:04 +0700

    Add the problem Water Bottles

---
+++ Water Bottles/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Water_Bottles
+{
+    public class Solution
+    {
+        int sum = 0;
+
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            return numBottles + Solve(numBottles, numExchange);
+        }
+        public int Solve(int num, int numExchange)
+        {
+            if (num < numExchange)
+                return 0;
+
+            return sum += num / numExchange + Solve((num / numExchange) + (num % numExchange), numExchange);
+        }
+    }
+}
