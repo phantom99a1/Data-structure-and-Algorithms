733d7cb1034902baf921950403bd44bd53512ac4
phantom99a1 <viendanho99a1@gmail.com>
2025-10-02 18:22:27 +0700

    Add the problem Water Bottles II

---
+++ Water Bottles II/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Water_Bottles_II
+{
+    public class Solution
+    {
+        public int MaxBottlesDrunk(int numBottles, int numExchange)
+        {
+            int totalDrunk = numBottles;
+
+            while (numBottles >= numExchange)
+            {
+                numBottles -= numExchange;
+                numExchange++;
+                totalDrunk++;
+                numBottles++; // drink one more bottle
+            }
+
+            return totalDrunk;
+        }
+    }
+}
