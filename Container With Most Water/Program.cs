50549d3eff48d385fc1006c4b2dc22cdf450480b
phantom99a1 <viendanho99a1@gmail.com>
2025-10-04 15:11:29 +0700

    Add the proble, Container With Most Water

---
+++ Container With Most Water/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Container_With_Most_Water
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = 0;
+
+            while (left < right)
+            {
+                int h = Math.Min(height[left], height[right]);
+                int w = right - left;
+                maxArea = Math.Max(maxArea, h * w);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
