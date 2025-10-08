f5d00fa165f4f849f42cf3ba61357a6d58dddda1
phantom99a1 <viendanho99a1@gmail.com>
2025-10-08 19:49:20 +0700

    Add the problem Successful Pairs of Spells and Potions

---
+++ Successful Pairs of Spells and Potions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Successful_Pairs_of_Spells_and_Potions
+{
+    public class Solution
+    {
+        public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {
+            var limit = 100001;
+            var prefixSum = new int[limit];
+            for (var i = 0; i < potions.Length; i++)
+            {
+                var neededSpellCost = success % potions[i] == 0
+                    ? success / potions[i]
+                    : success / potions[i] + 1;
+
+                if (neededSpellCost < limit)
+                    prefixSum[neededSpellCost]++;
+            }
+
+            for (var i = 1; i < prefixSum.Length; i++)
+                prefixSum[i] += prefixSum[i - 1];
+
+            var result = new int[spells.Length];
+            for (var i = 0; i < spells.Length; i++)
+                result[i] = prefixSum[spells[i]];
+
+            return result;
+        }
+    }
+}
