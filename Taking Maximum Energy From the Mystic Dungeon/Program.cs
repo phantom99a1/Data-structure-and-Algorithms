4ec0ce9d51bd739ff162b957f173fe1b2e299b2d
phantom99a1 <viendanho99a1@gmail.com>
2025-10-11 11:54:25 +0700

    Add the problem Taking Maximum Energy From the Mystic Dungeon

---
+++ Taking Maximum Energy From the Mystic Dungeon/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Taking_Maximum_Energy_From_the_Mystic_Dungeon
+{
+    public class Solution
+    {
+        public int MaximumEnergy(int[] energy, int k)
+        {
+            int ans = int.MinValue;
+            int n = energy.Length;
+
+            for (int i = n - k; i < n; ++i)
+            {
+                int j = i, sum = 0;
+                while (j >= 0)
+                {
+                    sum += energy[j];
+                    ans = Math.Max(ans, sum);
+                    j -= k;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
