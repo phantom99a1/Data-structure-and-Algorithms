21fc008ddbc2b2f0814655ab80e05ca601e1a5fc
phantom99a1 <viendanho99a1@gmail.com>
2025-10-13 22:09:00 +0700

    Add the problem Find Resultant Array After Removing Anagrams

---
+++ Find Resultant Array After Removing Anagrams/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Find_Resultant_Array_After_Removing_Anagrams
+{
+    public class Solution
+    {
+        public IList<string> RemoveAnagrams(string[] words)
+        {
+            List<string> result = new List<string>();
+            result.Add(words[0]);
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                string prevSorted = String.Concat(result[result.Count - 1].OrderBy(c => c));
+                string currSorted = String.Concat(words[i].OrderBy(c => c));
+
+                if (prevSorted != currSorted)
+                {
+                    result.Add(words[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
