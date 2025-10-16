3d17687b520ed7652b14f4885eef1e2620d49f71
phantom99a1 <viendanho99a1@gmail.com>
2025-10-16 23:19:19 +0700

    Add the problem Smallest Missing Non-negative Integer After Operations

---
+++ Smallest Missing Non-negative Integer After Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Smallest_Missing_Non_negative_Integer_After_Operations
+{
+    public class Solution
+    {
+        public int FindSmallestInteger(int[] nums, int value)
+        {
+            var count = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                int mod = ((num % value) + value) % value;
+                if (!count.ContainsKey(mod)) count[mod] = 0;
+                count[mod]++;
+            }
+
+            int[] freq = new int[value];
+            foreach (var kvp in count)
+            {
+                freq[kvp.Key] = kvp.Value;
+            }
+
+            int mex = 0;
+            while (true)
+            {
+                int mod = mex % value;
+                if (freq[mod] > 0)
+                {
+                    freq[mod]--;
+                    mex++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return mex;
+        }
+    }
+}
