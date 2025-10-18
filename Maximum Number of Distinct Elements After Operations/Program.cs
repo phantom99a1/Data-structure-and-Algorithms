c1830c17e1dc59bdf3e4a7e6086ea65c4c8533b9
phantom99a1 <viendanho99a1@gmail.com>
2025-10-18 10:37:21 +0700

    Add the problem Maximum Number of Distinct Elements After Operations

---
+++ Maximum Number of Distinct Elements After Operations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Maximum_Number_of_Distinct_Elements_After_Operations
+{
+    public class Solution
+    {
+        public int MaxDistinctElements(int[] nums, int k)
+        {
+            if (nums.Length == 0) return 0;
+            Array.Sort(nums);
+            int count = 0;
+            int prev = int.MinValue >> 1;
+            foreach (int a in nums)
+            {
+                int low = a - k;
+                int high = a + k;
+                int x = prev + 1;
+                if (x < low) x = low;
+                if (x <= high)
+                {
+                    count++;
+                    prev = x;
+                }
+            }
+            return count;
+        }
+    }
+}
