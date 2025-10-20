ebfb604eaa87106a051e2f5da516385e727895a0
phantom99a1 <viendanho99a1@gmail.com>
2025-10-20 19:40:50 +0700

    Add the problem Final Value of Variable After Performing Operations

---
+++ Final Value of Variable After Performing Operations/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Final_Value_of_Variable_After_Performing_Operations
+{
+    public class Solution
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int x = 0;
+            foreach (string op in operations)
+            {
+                if (op.Contains("+"))
+                {
+                    x++;
+                }
+                else
+                {
+                    x--;
+                }
+            }
+            return x;
+        }
+    }
+}
