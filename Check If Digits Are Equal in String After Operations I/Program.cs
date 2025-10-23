43be74391ed44977004e08b3f1a86f4c85518885
phantom99a1 <viendanho99a1@gmail.com>
2025-10-23 18:20:51 +0700

    Add the problem Check If Digits Are Equal in String After Operations I

---
+++ Check If Digits Are Equal in String After Operations I/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Check_If_Digits_Are_Equal_in_String_After_Operations_I
+{
+    public class Solution
+    {
+        public bool HasSameDigits(string s)
+        {
+            while (s.Length > 2)
+            {
+                var next = new StringBuilder();
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    int sum = (s[i] - '0' + s[i + 1] - '0') % 10;
+                    next.Append(sum);
+                }
+                s = next.ToString();
+            }
+            return s[0] == s[1];
+        }
+    }
+}
