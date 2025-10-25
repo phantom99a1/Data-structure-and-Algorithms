311005efc9d3e2c63440df3acd30779887826597
phantom99a1 <viendanho99a1@gmail.com>
2025-10-25 08:53:09 +0700

    Add the problem Calculate Money in Leetcode Bank

---
+++ Calculate Money in Leetcode Bank/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculate_Money_in_Leetcode_Bank
+{
+    internal class Program
+    {
+        public int TotalMoney(int n)
+        {
+            int weeks = n / 7;
+            int days = n % 7;
+            int total = 0;
+
+            // Sum for complete weeks
+            for (int i = 0; i < weeks; i++)
+            {
+                total += (7 * i) + (7 * (7 + 1)) / 2;
+            }
+
+            // Sum for remaining days
+            for (int i = 0; i < days; i++)
+            {
+                total += weeks + i + 1;
+            }
+
+            return total;
+        }
+    }
+}
