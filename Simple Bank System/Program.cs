e5242bec0c006586c760ff47ead4e097f9df8697
phantom99a1 <viendanho99a1@gmail.com>
2025-10-26 10:26:02 +0700

    Add the problem Simple Bank System

---
+++ Simple Bank System/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Simple_Bank_System
+{
+    public class Bank
+    {
+        private long[] balance;
+
+        public Bank(long[] balance)
+        {
+            this.balance = balance;
+        }
+
+        private bool IsValid(int account)
+        {
+            return account >= 1 && account <= balance.Length;
+        }
+
+        public bool Transfer(int account1, int account2, long money)
+        {
+            if (!IsValid(account1) || !IsValid(account2)) return false;
+            if (balance[account1 - 1] < money) return false;
+            balance[account1 - 1] -= money;
+            balance[account2 - 1] += money;
+            return true;
+        }
+
+        public bool Deposit(int account, long money)
+        {
+            if (!IsValid(account)) return false;
+            balance[account - 1] += money;
+            return true;
+        }
+
+        public bool Withdraw(int account, long money)
+        {
+            if (!IsValid(account)) return false;
+            if (balance[account - 1] < money) return false;
+            balance[account - 1] -= money;
+            return true;
+        }
+    }
+}
