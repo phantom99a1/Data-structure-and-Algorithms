93e7f1325ded207b76e538f4d3772d49b024a2ed
phantom99a1 <viendanho99a1@gmail.com>
2025-10-27 19:29:59 +0700

    Add the problem Number of Laser Beams in a Bank

---
+++ Number of Laser Beams in a Bank/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Number_of_Laser_Beams_in_a_Bank
+{
+    public class Solution
+    {
+        public int NumberOfBeams(string[] bank)
+        {
+            int prevCount = 0;
+            int totalBeams = 0;
+
+            foreach (string row in bank)
+            {
+                int currCount = row.Count(c => c == '1');
+                if (currCount > 0)
+                {
+                    totalBeams += prevCount * currCount;
+                    prevCount = currCount;
+                }
+            }
+
+            return totalBeams;
+        }
+    }
+}
