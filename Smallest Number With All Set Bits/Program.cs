786d0efa392133449dd13f3dd12bbfeb7376bdba
phantom99a1 <viendanho99a1@gmail.com>
2025-10-29 19:21:14 +0700

    Add the problem Smallest Number With All Set Bits

---
+++ Smallest Number With All Set Bits/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Smallest_Number_With_All_Set_Bits
+{
+    public class Solution
+    {
+        public int SmallestNumber(int n)
+        {
+            int k = 1;
+            while ((1 << k) - 1 < n)
+            {
+                k++;
+            }
+            return (1 << k) - 1;
+        }
+    }
+}
