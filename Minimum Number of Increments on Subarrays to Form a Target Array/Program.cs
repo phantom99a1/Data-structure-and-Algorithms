85e09ed58565c5c23a6442d68c3084da09bed302
phantom99a1 <viendanho99a1@gmail.com>
2025-10-30 20:22:37 +0700

    Add the problem Minimum Number of Increments on Subarrays to Form a Target Array

---
+++ Minimum Number of Increments on Subarrays to Form a Target Array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array
+{
+    public class Solution
+    {
+        public int MinNumberOperations(int[] target)
+        {
+            int operations = 0;
+            int prev = 0;
+
+            foreach (int num in target)
+            {
+                operations += Math.Max(0, num - prev);
+                prev = num;
+            }
+
+            return operations;
+        }
+    }
+}
