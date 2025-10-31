c19ee9da613e81dc76b8e14a6cfb0b92b5a85128
phantom99a1 <viendanho99a1@gmail.com>
2025-10-31 19:31:32 +0700

    Add the problem The Two Sneaky Numbers of Digitville

---
+++ The Two Sneaky Numbers of Digitville/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace The_Two_Sneaky_Numbers_of_Digitville
+{
+    public class Solution
+    {
+        public int[] GetSneakyNumbers(int[] nums)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+
+            foreach (int num in nums)
+            {
+                if (!count.ContainsKey(num)) count[num] = 0;
+                count[num]++;
+                if (count[num] == 2)
+                {
+                    result.Add(num);
+                    if (result.Count == 2) break;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
