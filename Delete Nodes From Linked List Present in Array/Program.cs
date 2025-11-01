ae6af21c512cff202f5747dcd0c62b26894a2f4a
phantom99a1 <viendanho99a1@gmail.com>
2025-11-01 14:01:13 +0700

    Add the problem Delete Nodes From Linked List Present in Array

---
+++ Delete Nodes From Linked List Present in Array/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Delete_Nodes_From_Linked_List_Present_in_Array
+{
+    public class Solution
+    {
+        public ListNode ModifiedList(int[] nums, ListNode head)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+
+            ListNode dummy = new ListNode(0, head);
+            ListNode pointer = dummy;
+
+            while (pointer.next != null)
+            {
+                if (set.Contains(pointer.next.val))
+                {
+                    pointer.next = pointer.next.next;
+                }
+                else
+                {
+                    pointer = pointer.next;
+                }
+            }
+            return dummy.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
