31a379208edf5a8ed479191a0e7065ca74a1fd7d
phantom99a1 <viendanho99a1@gmail.com>
2025-11-03 09:20:54 +0700

    Add the problem Minimum Time to Make Rope Colorful

---
+++ Minimum Time to Make Rope Colorful/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Minimum_Time_to_Make_Rope_Colorful
+{
+    internal class Program
+    {
+        public int MinCost(string colors, int[] neededTime)
+        {
+            int totalTime = 0;
+            for (int i = 1; i < colors.Length; i++)
+            {
+                if (colors[i] == colors[i - 1])
+                {
+                    totalTime += Math.Min(neededTime[i], neededTime[i - 1]);
+                    neededTime[i] = Math.Max(neededTime[i], neededTime[i - 1]);
+                }
+            }
+            return totalTime;
+        }
+    }
+}
