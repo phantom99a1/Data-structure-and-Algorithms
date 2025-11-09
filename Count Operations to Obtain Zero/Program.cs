c8d6492e009946b52335c854392e3eb86f29e049
phantom99a1 <viendanho99a1@gmail.com>
2025-11-09 09:54:09 +0700

    Add the problem Count Operations to Obtain Zero

---
+++ Count Operations to Obtain Zero/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Count_Operations_to_Obtain_Zero
+{
+    public class Solution
+    {
+        public int CountOperations(int num1, int num2)
+        {
+            int result = 0;
+            while (num1 > 0 && num2 > 0)
+            {
+                if (num1 >= num2)
+                {
+                    num1 -= num2;
+                }
+                else
+                {
+                    num2 -= num1;
+                }
+                result++;
+            }
+            return result;
+        }
+    }
+}
