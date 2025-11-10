7d1e7ba1017393a5056dd1030268cf62528f965d
phantom99a1 <viendanho99a1@gmail.com>
2025-11-10 19:00:53 +0700

    Add the problem Minimum Operations to Convert All Elements to Zero

---
+++ Minimum Operations to Convert All Elements to Zero/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Minimum_Operations_to_Convert_All_Elements_to_Zero
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            int operations = 0;
+
+            foreach (int num in nums)
+            {
+                while (stack.Count > 0 && stack.Peek() > num)
+                {
+                    stack.Pop();
+                }
+                if (stack.Count == 0 || stack.Peek() < num)
+                {
+                    operations++;
+                    stack.Push(num);
+                }
+            }
+
+            return operations;
+        }
+    }
+}
