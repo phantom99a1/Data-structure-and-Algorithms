0815707c434410979c6abe1b7e886739feaf59bb
phantom99a1 <viendanho99a1@gmail.com>
2025-11-11 23:55:36 +0700

    Add the problem Ones and Zeroes

---
+++ Ones and Zeroes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ones_and_Zeroes
+{
+    public class Solution
+    {
+        public int FindMaxForm(string[] strs, int m, int n)
+        {
+            int[,] dp = new int[m + 1, n + 1];
+
+            foreach (string str in strs)
+            {
+                int zeros = str.Count(c => c == '0');
+                int ones = str.Length - zeros;
+
+                for (int i = m; i >= zeros; i--)
+                {
+                    for (int j = n; j >= ones; j--)
+                    {
+                        dp[i, j] = Math.Max(dp[i, j], dp[i - zeros, j - ones] + 1);
+                    }
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
