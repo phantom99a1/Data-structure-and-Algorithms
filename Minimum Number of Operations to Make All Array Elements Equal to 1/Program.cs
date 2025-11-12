81bfed545f1d91093ca4e47caa1022f7e1b981f4
phantom99a1 <viendanho99a1@gmail.com>
2025-11-12 18:41:16 +0700

    Add the problem Minimum Number of Operations to Make All Array Elements Equal to 1

---
+++ Minimum Number of Operations to Make All Array Elements Equal to 1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            int n = nums.Length;
+            int ones = nums.Count(x => x == 1);
+            if (ones > 0) return n - ones;
+
+            int GCD(int a, int b) => b == 0 ? a : GCD(b, a % b);
+            int minLen = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int g = nums[i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    g = GCD(g, nums[j]);
+                    if (g == 1)
+                    {
+                        minLen = Math.Min(minLen, j - i);
+                        break;
+                    }
+                }
+            }
+
+            return minLen == int.MaxValue ? -1 : minLen + n - 1;
+        }
+    }
+}
