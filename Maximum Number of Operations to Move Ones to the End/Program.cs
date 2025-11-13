2f91c9ab8fc637203530850b087385deb0fb4072
phantom99a1 <viendanho99a1@gmail.com>
2025-11-13 23:37:54 +0700

    Add the problem Maximum Number of Operations to Move Ones to the End

---
+++ Maximum Number of Operations to Move Ones to the End/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Maximum_Number_of_Operations_to_Move_Ones_to_the_End
+{
+    public class Solution
+    {
+        public int MaxOperations(string s)
+        {
+            var count = 0;
+            var current = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1')
+                {
+                    current++;
+                }
+                else if (s[i] == '0')
+                {
+                    while (i < s.Length && s[i] == '0')
+                    {
+                        i++;
+                    }
+
+                    i--;
+                    count += current;
+                }
+            }
+
+            return count;
+        }
+    }
+}
