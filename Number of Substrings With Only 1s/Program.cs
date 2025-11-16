1a6ec00b22631e79a112d6dc4899875e4d71869b
phantom99a1 <viendanho99a1@gmail.com>
2025-11-16 09:53:40 +0700

    Add the problem Number of Substrings With Only 1s

---
+++ Number of Substrings With Only 1s/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Number_of_Substrings_With_Only_1s
+{
+    public class Solution
+    {
+        public int NumSub(string s)
+        {
+            int MOD = 1000000007;
+            long count = 0, result = 0;
+
+            foreach (char c in s)
+            {
+                if (c == '1')
+                {
+                    count++;
+                    result = (result + count) % MOD;
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+
+            return (int)result;
+        }
+    }
+}
