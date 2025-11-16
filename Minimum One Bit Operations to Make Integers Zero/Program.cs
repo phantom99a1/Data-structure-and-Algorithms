725f6fddb5f5c388a6ef12ebb9e593d24fbc2d54
phantom99a1 <viendanho99a1@gmail.com>
2025-11-16 23:49:21 +0700

    Add the problem Minimum One Bit Operations to Make Integers Zero

---
+++ Minimum One Bit Operations to Make Integers Zero/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Minimum_One_Bit_Operations_to_Make_Integers_Zero
+{
+    public class Solution
+    {
+        public int MinimumOneBitOperations(int n)
+        {
+            int res = 0;
+            while (n > 0)
+            {
+                res ^= n;
+                n >>= 1;
+            }
+            return res;
+        }
+    }
+}
