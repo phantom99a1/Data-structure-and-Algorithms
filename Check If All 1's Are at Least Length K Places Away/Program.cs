602a5e82ceb2513b51f77188f1b2949289b7835d
phantom99a1 <viendanho99a1@gmail.com>
2025-11-17 18:09:30 +0700

    Add the problem Check If All 1's Are at Least Length K Places Away

---
+++ Check If All 1's Are at Least Length K Places Away/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Check_If_All_1_s_Are_at_Least_Length_K_Places_Away
+{
+    public class Solution
+    {
+        public bool KLengthApart(int[] nums, int k)
+        {
+            int prev = -1; // store index of last seen 1
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    if (prev != -1 && i - prev - 1 < k)
+                    {
+                        return false;
+                    }
+                    prev = i;
+                }
+            }
+            return true;
+        }
+    }
+}
