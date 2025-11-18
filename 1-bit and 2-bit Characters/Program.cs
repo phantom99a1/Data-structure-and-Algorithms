917c681f2101ee08fabb9825d5849541fd6d0fbf
phantom99a1 <viendanho99a1@gmail.com>
2025-11-18 18:04:46 +0700

    Add the problem 1-bit and 2-bit Characters

---
+++ 1-bit and 2-bit Characters/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _1_bit_and_2_bit_Characters
+{
+    public class Solution
+    {
+        public bool IsOneBitCharacter(int[] bits)
+        {
+            int i = 0;
+            int n = bits.Length;
+
+            while (i < n - 1)
+            {
+                if (bits[i] == 1)
+                {
+                    i += 2; // two-bit character: 10 or 11
+                }
+                else
+                {
+                    i += 1; // one-bit character: 0
+                }
+            }
+
+            return i == n - 1;
+        }
+    }
+}
