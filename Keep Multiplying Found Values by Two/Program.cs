eb2edc62fb80d03696d946972ff851831261252a
phantom99a1 <viendanho99a1@gmail.com>
2025-11-19 17:58:43 +0700

    Add the problem Keep Multiplying Found Values by Two

---
+++ Keep Multiplying Found Values by Two/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Keep_Multiplying_Found_Values_by_Two
+{
+    public class Solution
+    {
+        public int FindFinalValue(int[] nums, int original)
+        {
+            while (true)
+            {
+                if (!nums.Contains(original)) break;
+                original *= 2;
+            }
+
+            return original;
+        }
+    }
+}
