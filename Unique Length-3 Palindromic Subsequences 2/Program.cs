4b750ed1e4484f92d3d95d71344f3d358b2ec221
phantom99a1 <viendanho99a1@gmail.com>
2025-11-21 22:35:00 +0700

    Add the problem Unique Length-3 Palindromic Subsequences 2

---
+++ Unique Length-3 Palindromic Subsequences 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Unique_Length_3_Palindromic_Subsequences_2
+{
+    public class Solution
+    {
+        public int CountPalindromicSubsequence(string s)
+        {
+            int result = 0;
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                int first = s.IndexOf(c);
+                int last = s.LastIndexOf(c);
+                if (first != -1 && last != -1 && first < last)
+                {
+                    HashSet<char> middle = new HashSet<char>();
+                    for (int i = first + 1; i < last; i++)
+                    {
+                        middle.Add(s[i]);
+                    }
+                    result += middle.Count;
+                }
+            }
+            return result;
+        }
+    }
+}
