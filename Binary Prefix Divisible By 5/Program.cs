2952801c837b0c9c7e96b7d67fa9649a572a7d4f
phantom99a1 <viendanho99a1@gmail.com>
2025-11-24 15:13:26 +0700

    Add the problem Binary Prefix Divisible By 5

---
+++ Binary Prefix Divisible By 5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Binary_Prefix_Divisible_By_5
+{
+    public class Solution
+    {
+        public IList<bool> PrefixesDivBy5(int[] nums)
+        {
+            var result = new List<bool>(nums.Length);
+            int mod = 0;
+            foreach (var bit in nums)
+            {
+                mod = (mod * 2 + bit) % 5;
+                result.Add(mod == 0);
+            }
+            return result;
+        }
+    }
+}
