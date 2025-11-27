d09e3df12d5359987edd7fce3885183aa24f19c1
phantom99a1 <viendanho99a1@gmail.com>
2025-11-27 18:58:23 +0700

    Add the problem Maximum Subarray Sum With Length Divisible by K

---
+++ Maximum Subarray Sum With Length Divisible by K/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Maximum_Subarray_Sum_With_Length_Divisible_by_K
+{
+    public class Solution
+    {
+        public long MaxSubarraySum(int[] nums, int k)
+        {
+            int n = nums.Length;
+            long prefix = 0;
+            long result = long.MinValue;
+
+            // Dictionary to store minimum prefix sum for each modulo class
+            var minPrefix = new Dictionary<int, long>();
+            minPrefix[0] = 0;  // base case
+
+            for (int i = 0; i < n; i++)
+            {
+                prefix += nums[i];
+                int mod = (i + 1) % k;
+
+                if (minPrefix.ContainsKey(mod))
+                {
+                    result = Math.Max(result, prefix - minPrefix[mod]);
+                    minPrefix[mod] = Math.Min(minPrefix[mod], prefix);
+                }
+                else
+                {
+                    minPrefix[mod] = prefix;
+                }
+            }
+
+            return result;
+        }
+    }
+}
