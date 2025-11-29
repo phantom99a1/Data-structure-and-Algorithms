b6b1ab508ba880d482271898f7917001f30a30fb
phantom99a1 <viendanho99a1@gmail.com>
2025-11-29 09:55:09 +0700

    Add the problem Minimum Operations to Make Array Sum Divisible by K

---
+++ Minimum Operations to Make Array Sum Divisible by K/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Minimum_Operations_to_Make_Array_Sum_Divisible_by_K
+{
+    public class Solution
+    {
+        public int MinOperations(int[] Nums, int K)
+        {
+            int Sum = 0;
+
+            for (int i = 0; i < Nums.Length; i++)
+            {
+                Sum += Nums[i];
+            }
+
+            return Sum % K;
+        }
+    }
+}
