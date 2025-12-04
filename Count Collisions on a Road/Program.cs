3ed4ca0b521bce58c23ae75228801dd34367eca4
phantom99a1 <viendanho99a1@gmail.com>
2025-12-04 19:30:11 +0700

    Add the problem Count Collisions on a Road

---
+++ Count Collisions on a Road/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Count_Collisions_on_a_Road
+{
+    public class Solution
+    {
+        public int CountCollisions(string directions)
+        {
+            int n = directions.Length;
+            int left = 0, right = n - 1;
+
+            // Skip cars moving left at the start
+            while (left < n && directions[left] == 'L')
+            {
+                left++;
+            }
+
+            // Skip cars moving right at the end
+            while (right >= 0 && directions[right] == 'R')
+            {
+                right--;
+            }
+
+            int collisions = 0;
+            for (int i = left; i <= right; i++)
+            {
+                if (directions[i] != 'S')
+                {
+                    collisions++;
+                }
+            }
+
+            return collisions;
+        }
+    }
+}
