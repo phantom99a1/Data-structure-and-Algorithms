4a4ee5509ccc534062b6db11c76c4ba9a3d13ced
phantom99a1 <viendanho99a1@gmail.com>
2025-12-05 17:41:45 +0700

    Add the problem Count Partitions with Even Sum Difference

---
+++ Count Partitions with Even Sum Difference/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Count_Partitions_with_Even_Sum_Difference
+{
+    public class Solution
+    {
+        public int CountPartitions(int[] nums)
+        {
+            int n = nums.Length;
+            int total = 0;
+
+            foreach (int num in nums)
+            {
+                total += num;
+            }
+
+            // If total sum is even, all (n-1) partitions are valid
+            if (total % 2 == 0)
+            {
+                return n - 1;
+            }
+
+            // Otherwise, no partition is valid
+            return 0;
+        }
+    }
+}
