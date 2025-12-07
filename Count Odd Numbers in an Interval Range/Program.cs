1168ca5723368c0f201a63aa9c82f38a50b2c45b
phantom99a1 <viendanho99a1@gmail.com>
2025-12-07 10:25:10 +0700

    Add the problem Count Odd Numbers in an Interval Range

---
+++ Count Odd Numbers in an Interval Range/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Count_Odd_Numbers_in_an_Interval_Range
+{
+    public class Solution
+    {
+        public int CountOdds(int low, int high)
+        {
+            int count = (high - low) / 2;
+
+            // If either boundary is odd, add 1
+            if (low % 2 != 0 || high % 2 != 0)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
