59a13ef2040dbea134005c5a9ba2012369cf97c9
phantom99a1 <viendanho99a1@gmail.com>
2025-12-08 19:50:15 +0700

    Add the problem Count Square Sum Triples

---
+++ Count Square Sum Triples/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Count_Square_Sum_Triples
+{
+    public class Solution
+    {
+        public int CountTriples(int n)
+        {
+            int count = 0;
+
+            for (int a = 1; a <= n; a++)
+            {
+                for (int b = 1; b <= n; b++)
+                {
+                    int sum = a * a + b * b;
+                    int c = (int)Math.Sqrt(sum);
+
+                    if (c <= n && c * c == sum)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
