bcc6cc712007db985a90f25f0594f8dfa1826d67
phantom99a1 <viendanho99a1@gmail.com>
2025-12-15 22:13:55 +0700

    Add the problem Number of Smooth Descent Periods of a Stock

---
+++ Number of Smooth Descent Periods of a Stock/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Number_of_Smooth_Descent_Periods_of_a_Stock
+{
+    public class Solution
+    {
+        public long GetDescentPeriods(int[] prices)
+        {
+            long result = 1;
+            long count = 1;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i - 1] - prices[i] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+                result += count;
+            }
+
+            return result;
+        }
+    }
+}
