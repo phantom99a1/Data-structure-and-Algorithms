80a53609f2aa81f8a51b6997fe05f7eeeeaf786f
phantom99a1 <viendanho99a1@gmail.com>
2025-12-16 18:45:03 +0700

    Add the problem Maximum Profit from Trading Stocks with Discounts

---
+++ Maximum Profit from Trading Stocks with Discounts/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Maximum_Profit_from_Trading_Stocks_with_Discounts
+{
+    public class Solution
+    {
+        public int MaxProfit(int n, int[] present, int[] future, int[][] hierarchy,
+                             int budget)
+        {
+            List<int>[] g = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                g[i] = new List<int>();
+            }
+            foreach (var e in hierarchy)
+            {
+                g[e[0] - 1].Add(e[1] - 1);
+            }
+
+            (int[] dp0, int[] dp1, int size) Dfs(int u)
+            {
+                int cost = present[u];
+                int dCost = present[u] / 2;  // discounted cost
+
+                // dp[u][state][budget]
+                // state = 0: Do not purchase parent node, state = 1: Must purchase
+                // parent node
+                int[] dp0 = new int[budget + 1];
+                int[] dp1 = new int[budget + 1];
+                // subProfit[state][budget]
+                // state = 0: discount not available, state = 1: discount available
+                int[] subProfit0 = new int[budget + 1];
+                int[] subProfit1 = new int[budget + 1];
+                int uSize = cost;
+
+                foreach (int v in g[u])
+                {
+                    var (childDp0, childDp1, vSize) = Dfs(v);
+                    uSize += vSize;
+                    for (int i = budget; i >= 0; i--)
+                    {
+                        for (int sub = 0; sub <= Math.Min(vSize, i); sub++)
+                        {
+                            if (i - sub >= 0)
+                            {
+                                subProfit0[i] =
+                                    Math.Max(subProfit0[i],
+                                             subProfit0[i - sub] + childDp0[sub]);
+                                subProfit1[i] =
+                                    Math.Max(subProfit1[i],
+                                             subProfit1[i - sub] + childDp1[sub]);
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i <= budget; i++)
+                {
+                    dp0[i] = subProfit0[i];
+                    dp1[i] = subProfit0[i];
+                    if (i >= dCost)
+                    {
+                        dp1[i] = Math.Max(subProfit0[i], subProfit1[i - dCost] +
+                                                             future[u] - dCost);
+                    }
+                    if (i >= cost)
+                    {
+                        dp0[i] = Math.Max(subProfit0[i],
+                                          subProfit1[i - cost] + future[u] - cost);
+                    }
+                }
+
+                return (dp0, dp1, uSize);
+            }
+
+            return Dfs(0).dp0[budget];
+        }
+    }
+}
