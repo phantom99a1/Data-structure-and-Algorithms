fb8057d4c217269d2e8de915c3e362d396c597de
phantom99a1 <viendanho99a1@gmail.com>
2025-12-20 20:07:32 +0700

    Add the problem Delete Columns to Make Sorted

---
+++ Delete Columns to Make Sorted/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Delete_Columns_to_Make_Sorted
+{
+    public class Solution
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int n = strs.Length;
+            int stringSize = strs[0].Length;
+            int result = 0;
+
+            for (int j = 0; j < stringSize; j++)
+            {
+                char prev = strs[0][j];
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (strs[i][j] < prev)
+                    {
+                        result++;
+                        break;
+                    }
+                    prev = strs[i][j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
