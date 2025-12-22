13515bf9c42f11646fc73a2e7a87a30a523fe409
phantom99a1 <viendanho99a1@gmail.com>
2025-12-22 22:26:53 +0700

    Add the problem Delete Columns to Make Sorted III

---
+++ Delete Columns to Make Sorted III/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Delete_Columns_to_Make_Sorted_III
+{
+    public class Solution
+    {
+        public int MinDeletionSize(string[] strs)
+        {
+            int rows = strs.Length;
+            int cols = strs[0].Length;
+
+            int[] dp = new int[cols];
+            Array.Fill(dp, 1);
+
+            for (int c = 1; c < cols; c++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    bool valid = true;
+
+                    for (int r = 0; r < rows; r++)
+                    {
+                        if (strs[r][j] > strs[r][c])
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+
+                    if (valid)
+                    {
+                        dp[c] = Math.Max(dp[c], dp[j] + 1);
+                    }
+                }
+            }
+
+            return cols - dp.Max();
+        }
+    }
+}
