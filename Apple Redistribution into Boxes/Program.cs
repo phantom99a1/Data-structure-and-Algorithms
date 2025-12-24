05644e3845932267fa8875c90d3ee8be34862613
phantom99a1 <viendanho99a1@gmail.com>
2025-12-24 19:04:47 +0700

    Add the problem Apple Redistribution into Boxes

---
+++ Apple Redistribution into Boxes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Apple_Redistribution_into_Boxes
+{
+    public class Solution
+    {
+        public int MinimumBoxes(int[] apple, int[] capacity)
+        {
+            int total = 0;
+            foreach (int a in apple) total += a;
+
+            Array.Sort(capacity);
+            Array.Reverse(capacity);
+
+            int curr = 0;
+            int count = 0;
+
+            foreach (int cap in capacity)
+            {
+                curr += cap;
+                count++;
+                if (curr >= total) break;
+            }
+
+            return count;
+        }
+    }
+}
