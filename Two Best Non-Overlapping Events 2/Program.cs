72814f825034e26fc5dc948a498e87cb09bce094
phantom99a1 <viendanho99a1@gmail.com>
2025-12-25 23:48:03 +0700

    Add the problem Two Best Non-Overlapping Events version 2

---
+++ Two Best Non-Overlapping Events 2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Two_Best_Non_Overlapping_Events_2
+{
+    public class Solution
+    {
+        public int MaxTwoEvents(int[][] events)
+        {
+            Array.Sort(events, (a, b) => a[0].CompareTo(b[0]));
+
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>();
+
+            int maxValueSoFar = 0;
+            int result = 0;
+
+            foreach (var e in events)
+            {
+                int start = e[0];
+                int end = e[1];
+                int value = e[2];
+
+                while (pq.Count > 0 && pq.Peek()[0] < start)
+                {
+                    maxValueSoFar = Math.Max(maxValueSoFar, pq.Dequeue()[1]);
+                }
+
+                result = Math.Max(result, maxValueSoFar + value);
+                pq.Enqueue(new int[] { end, value }, end);
+            }
+
+            return result;
+        }
+    }
+}
