348831524df759422825c18ce97afcfc2d33b186
phantom99a1 <viendanho99a1@gmail.com>
2025-12-25 17:55:27 +0700

    Add the problem Maximize Happiness of Selected Children

---
+++ Maximize Happiness of Selected Children/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Maximize_Happiness_of_Selected_Children
+{
+    public class Solution
+    {
+        public long MaximumHappinessSum(int[] happiness, int k)
+        {
+            Array.Sort(happiness);
+
+            long result = 0;
+            int decrease = 0;
+            int n = happiness.Length;
+            for (int i = n - 1; i >= n - k; i--)
+            {
+                int current = happiness[i] - decrease;
+                if (current > 0)
+                {
+                    result += current;
+                }
+                decrease++;
+            }
+
+            return result;
+        }
+    }
+}
