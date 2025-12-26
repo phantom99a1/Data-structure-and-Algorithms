3db5df105deac62b975c37295639f01ecacbd7ba
phantom99a1 <viendanho99a1@gmail.com>
2025-12-26 17:19:36 +0700

    Add the problem Minimum Penalty for a Shop

---
+++ Minimum Penalty for a Shop/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Minimum_Penalty_for_a_Shop
+{
+    public class Solution
+    {
+        public int BestClosingTime(string s)
+        {
+            int pen = 0;
+            foreach (char c in s)
+                if (c == 'Y') pen++;
+
+            int best = pen, ans = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'Y') pen--;
+                else pen++;
+
+                if (pen < best)
+                {
+                    best = pen;
+                    ans = i + 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
