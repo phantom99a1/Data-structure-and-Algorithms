833dad8c0cdac3d87c95a73f41b737213c74c07b
phantom99a1 <viendanho99a1@gmail.com>
2025-12-29 14:36:18 +0700

    Add the problem Count Negative Numbers in a Sorted Matrix

---
+++ Count Negative Numbers in a Sorted Matrix/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Count_Negative_Numbers_in_a_Sorted_Matrix
+{
+    public class Solution
+    {
+        public int CountNegatives(int[][] grid)
+        {
+            int result = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = grid[0].Length - 1; j >= 0 && grid[i][j] < 0; j--)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
