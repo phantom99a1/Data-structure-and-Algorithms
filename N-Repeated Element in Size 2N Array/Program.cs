09487c42ce3a5239b80d8aafdb5b6fb6fbb8ab72
phantom99a1 <viendanho99a1@gmail.com>
2026-01-02 17:16:19 +0700

    Add the problem N-Repeated Element in Size 2N Array

---
+++ N-Repeated Element in Size 2N Array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace N_Repeated_Element_in_Size_2N_Array
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach (var x in nums)
+            {
+                if (!set.Add(x)) return x;
+            }
+            return -1;
+        }
+    }
+}
