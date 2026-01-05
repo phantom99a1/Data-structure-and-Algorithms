e34bf559399e4f3a95bd5eae552a134748cd729d
phantom99a1 <viendanho99a1@gmail.com>
2026-01-05 21:39:03 +0700

    Add the problem Maximum Matrix Sum version 2

---
+++ Maximum Matrix Sum ver2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Maximum_Matrix_Sum_ver2
+{
+    class Solution
+    {
+        public long MaxMatrixSum(int[][] matrix)
+        {
+            int min = int.MaxValue;
+            long sum = 0;
+            int negCount = 0;
+            for (int i = 0; i < matrix.Length; i++)
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] < 0)
+                        negCount++;
+                    int ab = Math.Abs(matrix[i][j]);
+                    min = Math.Min(min, ab);
+                    sum += ab;
+                }
+            if (negCount % 2 == 0)
+                return sum;
+            return sum - 2 * min;
+        }
+    }
+}
