6325c903fb1b696256da9e035ba93c48be6ac507
phantom99a1 <viendanho99a1@gmail.com>
2026-01-10 15:45:09 +0700

    Add the problem Minimum ASCII Delete Sum for Two Strings

---
+++ Minimum ASCII Delete Sum for Two Strings/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Minimum_ASCII_Delete_Sum_for_Two_Strings
+{
+    public class Solution
+    {
+        public int MinimumDeleteSum(string s1, string s2)
+        {
+            int m = s1.Length;
+            int n = s2.Length;
+
+            int[,] dp = new int[m + 1, n + 1];
+
+            // Initialize base cases
+            for (int i = 1; i <= m; i++)
+            {
+                dp[i, 0] = dp[i - 1, 0] + s1[i - 1];
+            }
+
+            for (int j = 1; j <= n; j++)
+            {
+                dp[0, j] = dp[0, j - 1] + s2[j - 1];
+            }
+
+            // Fill the dp array
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (s1[i - 1] == s2[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Min(dp[i - 1, j] + s1[i - 1], dp[i, j - 1] + s2[j - 1]);
+                    }
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
