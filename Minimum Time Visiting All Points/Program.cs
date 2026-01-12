5dc68c6fc70f4ebc17bf4ebc7768b81a1c3fa060
phantom99a1 <viendanho99a1@gmail.com>
2026-01-12 15:33:07 +0700

    Add the problem Minimum Time Visiting All Points

---
+++ Minimum Time Visiting All Points/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Minimum_Time_Visiting_All_Points
+{
+    public class Solution
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            int ans = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                ans += Math.Max(Math.Abs(points[i][0] - points[i - 1][0]), Math.Abs(points[i][1] - points[i - 1][1]));
+            }
+            return ans;
+        }
+    }
+}
