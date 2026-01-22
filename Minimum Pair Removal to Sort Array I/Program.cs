fb6520e0c05bb0b56b54ab919ae39442bf85aeeb
phantom99a1 <viendanho99a1@gmail.com>
2026-01-22 19:41:31 +0700

    Add the problem Minimum Pair Removal to Sort Array I

---
+++ Minimum Pair Removal to Sort Array I/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Minimum_Pair_Removal_to_Sort_Array_I
+{
+    public class Solution
+    {
+        bool IsSorted(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count - 1; i++)
+            {
+                if (nums[i + 1] >= nums[i]) continue;
+                return false;
+            }
+            return true;
+        }
+        public int MinimumPairRemoval(int[] nums)
+        {
+            List<int> temp = new(nums);
+            int count = 0;
+            while (!IsSorted(temp))
+            {
+                int idx = FindMinIdx(temp);
+                count++;
+                temp[idx] = temp[idx] + temp[idx + 1];
+                temp.RemoveAt(idx + 1);
+            }
+            return count;
+        }
+        int FindMinIdx(List<int> temp)
+        {
+            int cur = -1, min = int.MaxValue;
+            for (int i = 0; i < temp.Count - 1; i++)
+            {
+                if (min > (temp[i] + temp[i + 1]))
+                {
+                    min = temp[i] + temp[i + 1];
+                    cur = i;
+                }
+            }
+            return cur;
+        }
+    }
+}
