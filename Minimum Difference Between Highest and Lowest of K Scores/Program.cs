813f0737411a23c50211bf15b87dbd377146a2b8
phantom99a1 <viendanho99a1@gmail.com>
2026-01-25 11:23:02 +0700

    Add the problem Minimum Difference Between Highest and Lowest of K Scores

---
+++ Minimum Difference Between Highest and Lowest of K Scores/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores
+{
+    public class Solution
+    {
+        public int MinimumDifference(int[] InputNumbers, int GroupSize)
+        {
+            if (InputNumbers.Length == 1)
+            {
+                return 0;
+            }
+
+            Array.Sort(InputNumbers, (First, Second) => Second - First);
+            int MinDifference = int.MaxValue;
+
+            for (int Index = 0; Index <= InputNumbers.Length - GroupSize; Index++)
+            {
+                MinDifference = Math.Min(MinDifference, InputNumbers[Index] - InputNumbers[Index + GroupSize - 1]);
+            }
+
+            return MinDifference;
+        }
+    }
+}
