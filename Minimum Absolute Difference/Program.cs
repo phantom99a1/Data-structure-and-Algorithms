e27f51c75b03a0d8245b52b998e8e4a5cd669a4c
phantom99a1 <viendanho99a1@gmail.com>
2026-01-26 19:53:54 +0700

    Add the problem Minimum Absolute Difference

---
+++ Minimum Absolute Difference/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Minimum_Absolute_Difference
+{
+    public class Solution
+    {
+        public IList<IList<int>> MinimumAbsDifference(int[] InputArray)
+        {
+            int MinValue = InputArray[0];
+            int MaxValue = InputArray[0];
+
+            for (int Index = 1; Index < InputArray.Length; ++Index)
+            {
+                int CurrentNumber = InputArray[Index];
+                MinValue = Math.Min(MinValue, CurrentNumber);
+                MaxValue = Math.Max(MaxValue, CurrentNumber);
+            }
+
+            int ValueRange = MaxValue - MinValue;
+            bool[] NumberExists = new bool[ValueRange + 1];
+
+            for (int Index = 0; Index < InputArray.Length; ++Index)
+            {
+                NumberExists[InputArray[Index] - MinValue] = true;
+            }
+
+            var ResultPairs = new List<IList<int>>();
+            int PreviousValue = MinValue;
+            int MinDifference = ValueRange;
+
+            for (int Offset = 1; Offset < NumberExists.Length; ++Offset)
+            {
+                if (!NumberExists[Offset])
+                {
+                    continue;
+                }
+
+                int CurrentValue = MinValue + Offset;
+                int CurrentDifference = CurrentValue - PreviousValue;
+                int LeftValue = PreviousValue;
+
+                PreviousValue = CurrentValue;
+
+                if (CurrentDifference > MinDifference)
+                {
+                    continue;
+                }
+
+                if (CurrentDifference < MinDifference)
+                {
+                    MinDifference = CurrentDifference;
+                    ResultPairs.Clear();
+                }
+
+                ResultPairs.Add(new int[] { LeftValue, CurrentValue });
+            }
+
+            return ResultPairs;
+        }
+    }
+}
