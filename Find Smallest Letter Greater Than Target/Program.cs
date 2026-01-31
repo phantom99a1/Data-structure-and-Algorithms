92d4a2c386c3f38b73ea27340f8d53eb58cb83c2
phantom99a1 <viendanho99a1@gmail.com>
2026-01-31 17:13:03 +0700

    Add the problem Find Smallest Letter Greater Than Target

---
+++ Find Smallest Letter Greater Than Target/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Find_Smallest_Letter_Greater_Than_Target
+{
+    public class Solution
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            int[] freq = new int[26];
+            for (int i = 0; i < letters.Length; i++)
+                freq[letters[i] - 'a']++;
+            int th = target - 'a';
+            for (int i = 0; i < freq.Length; i++)
+                if (i > th && freq[i] != 0)
+                    return (char)(i + 'a');
+            return letters[0];
+        }
+    }
+}
