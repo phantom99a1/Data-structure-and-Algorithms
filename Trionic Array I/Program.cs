77b789cd69578d6bd732f01dab93e35d119031ff
phantom99a1 <viendanho99a1@gmail.com>
2026-02-03 21:33:18 +0700

    Add the problem Trionic Array I

---
+++ Trionic Array I/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Trionic_Array_I
+{
+    public class Solution
+    {
+        public bool IsTrionic(int[] nums)
+        {
+            int n = nums.Length, i = 0;
+            while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+            if (i == 0 || i == n - 1) return false;
+
+            int p = i;
+            while (i + 1 < n && nums[i] > nums[i + 1]) i++;
+            if (i == p || i == n - 1) return false;
+
+            while (i + 1 < n && nums[i] < nums[i + 1]) i++;
+            return i == n - 1;
+        }
+    }
+}
