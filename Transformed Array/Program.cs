721ca9cbcead011bae4f9d5d0f52b21a93ca6b17
phantom99a1 <viendanho99a1@gmail.com>
2026-02-05 21:53:14 +0700

    Add the problem Transformed Array

---
+++ Transformed Array/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Transformed_Array
+{
+    public class Solution
+    {
+        public int[] ConstructTransformedArray(int[] nums)
+        {
+            int n = nums.Length;
+            int[] ans = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = nums[((i + nums[i]) % n + n) % n];
+            }
+            return ans;
+        }
+    }
+}
