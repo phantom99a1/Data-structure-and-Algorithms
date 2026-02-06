0906fca49094194907a60ff793f424b895f619b1
phantom99a1 <viendanho99a1@gmail.com>
2026-02-06 19:36:30 +0700

    Add the problem Minimum Removals to Balance Array

---
+++ Minimum Removals to Balance Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Minimum_Removals_to_Balance_Array
+{
+    public class Solution
+    {
+        public int MinRemoval(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int i = 0;
+            int maxLen = 0;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                while ((long)nums[j] > (long)nums[i] * k)
+                {
+                    i++;
+                }
+                maxLen = Math.Max(maxLen, j - i + 1);
+            }
+
+            return nums.Length - maxLen;
+        }
+    }
+}
