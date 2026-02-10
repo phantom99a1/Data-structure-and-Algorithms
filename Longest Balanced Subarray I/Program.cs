49314587bd4339bd9250c7c9097652330528ee60
phantom99a1 <viendanho99a1@gmail.com>
2026-02-10 18:34:56 +0700

    Add the problem Longest Balanced Subarray I

---
+++ Longest Balanced Subarray I/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Longest_Balanced_Subarray_I
+{
+    public class Solution
+    {
+        public int LongestBalanced(int[] nums)
+        {
+            int len = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var odd = new Dictionary<int, int>();
+                var even = new Dictionary<int, int>();
+
+                for (int j = i; j < nums.Length; j++)
+                {
+                    var dict = (nums[j] & 1) == 1 ? odd : even;
+                    dict[nums[j]] = dict.GetValueOrDefault(nums[j]) + 1;
+
+                    if (odd.Count == even.Count)
+                    {
+                        len = Math.Max(len, j - i + 1);
+                    }
+                }
+            }
+
+            return len;
+        }
+    }
+}
