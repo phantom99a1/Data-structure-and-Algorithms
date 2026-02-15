22bd7ca8687191b12c9a4c0d45d735929715dd9e
phantom99a1 <viendanho99a1@gmail.com>
2026-02-15 10:32:55 +0700

    Add the problem Add Binary

---
+++ Add Binary/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Add_Binary
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            var sum = new List<int>();
+            for (int i = a.Length - 1, j = b.Length - 1, carry = 0; i >= 0 || j >= 0 || carry > 0;)
+            {
+                var firstDigit = i >= 0 ? a[i--] - '0' : 0;
+                var secondDigit = j >= 0 ? b[j--] - '0' : 0;
+                var sumDigit = firstDigit + secondDigit + carry;
+                carry = sumDigit / 2;
+                sum.Add(sumDigit % 2);
+            }
+            sum.Reverse();
+            return String.Concat(sum);
+        }
+    }
+}
