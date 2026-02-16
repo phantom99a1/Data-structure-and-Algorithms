2661e8ed6a6cccf496d6b9b7871e265cf3f8adc3
phantom99a1 <viendanho99a1@gmail.com>
2026-02-16 08:46:41 +0700

    Add the problem Reverse Bits

---
+++ Reverse Bits/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Reverse_Bits
+{
+    public class Solution
+    {
+        public int ReverseBits(int n)
+        {
+            int res = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                res = res << 1;        // Shift res left
+                res += (n & 1);        // Add the least significant bit of n to res
+                n = n >> 1;            // Shift n to the right
+            }
+            return res;
+        }
+    }
+}
