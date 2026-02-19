eedbb0ad13a463a9f8e10427ea3e969e9a3d3318
phantom99a1 <viendanho99a1@gmail.com>
2026-02-19 15:10:57 +0700

    Add the problem Count Binary Substrings

---
+++ Count Binary Substrings/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Count_Binary_Substrings
+{
+    public class Solution
+    {
+        public int CountBinarySubstrings(string InputString)
+        {
+            int PreviousRunLength = 0;
+            int CurrentRunLength = 1;
+            int TotalSubstrings = 0;
+
+            for (int Index = 1; Index < InputString.Length; Index++)
+            {
+                if (InputString[Index] == InputString[Index - 1])
+                    CurrentRunLength++;
+                else
+                {
+                    PreviousRunLength = CurrentRunLength;
+                    CurrentRunLength = 1;
+                }
+
+                if (PreviousRunLength >= CurrentRunLength)
+                    TotalSubstrings++;
+            }
+
+            return TotalSubstrings;
+        }
+    }
+}
