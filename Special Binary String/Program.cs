d74059d9ecacf9b552b34c603d144e0738b7f8c8
phantom99a1 <viendanho99a1@gmail.com>
2026-02-20 10:33:15 +0700

    Add the problem Special Binary String

---
+++ Special Binary String/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Special_Binary_String
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public string MakeLargestSpecial(string InputString)
+        {
+            List<string> SpecialSubstrings = new List<string>();
+            int BalanceCount = 0;
+            int StartIndex = 0;
+
+            for (int Index = 0; Index < InputString.Length; Index++)
+            {
+                if (InputString[Index] == '1')
+                    BalanceCount++;
+                else
+                    BalanceCount--;
+
+                if (BalanceCount == 0)
+                {
+                    string InnerSubstring = InputString.Substring(StartIndex + 1, Index - StartIndex - 1);
+                    InnerSubstring = MakeLargestSpecial(InnerSubstring);
+                    SpecialSubstrings.Add("1" + InnerSubstring + "0");
+                    StartIndex = Index + 1;
+                }
+            }
+
+            SpecialSubstrings.Sort((First, Second) => string.Compare(Second, First, StringComparison.Ordinal));
+
+            return string.Join("", SpecialSubstrings);
+        }
+    }
+}
