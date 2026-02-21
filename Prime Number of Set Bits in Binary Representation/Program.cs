584546cff1b27a8483edd6cbe412373c9e841089
phantom99a1 <viendanho99a1@gmail.com>
2026-02-21 10:21:47 +0700

    Add the problem Prime Number of Set Bits in Binary Representation

---
+++ Prime Number of Set Bits in Binary Representation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Prime_Number_of_Set_Bits_in_Binary_Representation
+{
+    public class Solution
+    {
+        public int CountPrimeSetBits(int left, int right)
+        {
+            var count = 0;
+            for (int i = left; i <= right; i++)
+            {
+                var bits = CountBits(i);
+                if (bits == 2 || bits == 3 || bits == 5 || bits == 7 ||
+                    bits == 11 || bits == 13 || bits == 17 || bits == 19)
+                    count++;
+            }
+
+            return count;
+        }
+
+        private int CountBits(int num)
+        {
+            var count = 0;
+            while (num > 0)
+            {
+                num &= num - 1;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
