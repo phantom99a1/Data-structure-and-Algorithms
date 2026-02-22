eae26517ccb4c85924b211b1f3ab5ce20ae3b67c
phantom99a1 <viendanho99a1@gmail.com>
2026-02-22 15:08:22 +0700

    Add the problem Binary Gap

---
+++ Binary Gap/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Binary_Gap
+{
+    public class Solution
+    {
+        public int BinaryGap(int n)
+        {
+            var last = -1;
+            var result = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if (((n >> i) & 1) == 1)
+                {
+                    if (last >= 0)
+                        result = Math.Max(result, i - last);
+                    last = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
