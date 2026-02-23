578233de7f37f05a940424e8b03498cef7bdb9de
phantom99a1 <viendanho99a1@gmail.com>
2026-02-23 14:11:34 +0700

    Add the problem Check If a String Contains All Binary Codes of Size K

---
+++ Check If a String Contains All Binary Codes of Size K/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Check_If_a_String_Contains_All_Binary_Codes_of_Size_K
+{
+    public class Solution
+    {
+        public bool HasAllCodes(string s, int k)
+        {
+            int req = 1 << k;
+            bool[] seen = new bool[req];
+            int mask = req - 1;
+            int hash = 0;
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                hash = ((hash << 1) & mask) | (s[i] & 1);
+
+                if (i >= k - 1 && !seen[hash])
+                {
+                    seen[hash] = true;
+                    req--;
+                    if (req == 0) return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
