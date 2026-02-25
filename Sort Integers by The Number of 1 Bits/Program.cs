ee32d7d896111f7c4bfdf0e623520a087854d422
phantom99a1 <viendanho99a1@gmail.com>
2026-02-25 18:28:50 +0700

    Add the problem Sort Integers by The Number of 1 Bits

---
+++ Sort Integers by The Number of 1 Bits/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sort_Integers_by_The_Number_of_1_Bits
+{
+    public class Solution
+    {
+        public int[] SortByBits(int[] arr)
+        {
+            Array.Sort(arr, (a, b) => {
+                var b1 = int.PopCount(a);
+                var b2 = int.PopCount(b);
+
+                return b1 == b2 ? a - b : b1 - b2;
+            });
+
+            return arr;
+        }
+    }
+}
