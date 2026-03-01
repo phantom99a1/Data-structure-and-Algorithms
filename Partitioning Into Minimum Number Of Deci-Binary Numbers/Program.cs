096f0eeb9f97cba383aa7dd5fe1286af8a5f5bf3
phantom99a1 <viendanho99a1@gmail.com>
2026-03-01 13:53:39 +0700

    Add the problem Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ Partitioning Into Minimum Number Of Deci-Binary Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Partitioning_Into_Minimum_Number_Of_Deci_Binary_Numbers
+{
+    public class Solution
+    {
+        public int MinPartitions(string n) => n.
+            ToCharArray().
+            Max(m => m - '0');
+    }
+}
