d00b01fb254ecd8a0345d02706a08a528b0be2a5
phantom99a1 <viendanho99a1@gmail.com>
2026-03-02 19:28:53 +0700

    Add the problem Number of Steps to Reduce a Number in Binary Representation to One

---
+++ Number of Steps to Reduce a Number in Binary Representation to One/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One
+{
+    public class Solution
+    {
+        public int NumSteps(string s)
+        {
+            int steps = 0, carry = 0;
+            for (int i = s.Length - 1; i > 0; i--)
+            {
+                if ((s[i] == '1' ? 1 : 0) + carry == 1)
+                {
+                    steps += 2;
+                    carry = 1;
+                }
+                else
+                    steps += 1;
+            }
+
+            return steps + carry;
+        }
+    }
+}
