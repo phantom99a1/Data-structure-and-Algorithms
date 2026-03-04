eda4b505e4577c2a7d974d085227ef0a21120d0e
phantom99a1 <viendanho99a1@gmail.com>
2026-03-04 18:59:45 +0700

    Add the problem Special Positions in a Binary Matrix

---
+++ Special Positions in a Binary Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Special_Positions_in_a_Binary_Matrix
+{
+    public class Solution
+    {
+        public int NumSpecial(int[][] mat)
+        {
+            int result = 0;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                for (int j = 0; j < mat[i].Length; j++)
+                {
+                    if (mat[i][j] == 1)
+                    {
+                        int sumx = 0;
+                        int sumy = 0;
+                        for (int m = 0; m < mat[i].Length; m++)
+                        {
+                            sumx += mat[i][m];
+                        }
+                        for (int k = 0; k < mat.Length; k++)
+                        {
+                            sumy += mat[k][j];
+                        }
+                        if (sumx == 1 && sumy == 1)
+                        {
+                            result++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
