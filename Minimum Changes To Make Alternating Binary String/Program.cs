725d342b112d87bcdeaf0b04b7d28824a1b83207
phantom99a1 <viendanho99a1@gmail.com>
2026-03-05 19:41:47 +0700

    Add the problem Minimum Changes To Make Alternating Binary String

---
+++ Minimum Changes To Make Alternating Binary String/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Minimum_Changes_To_Make_Alternating_Binary_String
+{
+    public class Solution
+    {
+        public int MinOperations(string s)
+        {
+            int n, c = 0, j = 0;
+            for (n = 0; n < s.Length; n++, j ^= 1)
+            {
+                if (s[n] - '0' == j) c++;
+            }
+            return Math.Min(c, n - c);
+        }
+    }
+}
