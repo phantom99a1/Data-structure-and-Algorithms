bf243271b866215e2def8777acbc4d574c1a92cc
phantom99a1 <viendanho99a1@gmail.com>
2026-03-06 23:24:18 +0700

    Add the problem Check if Binary String Has at Most One Segment of Ones

---
+++ Check if Binary String Has at Most One Segment of Ones/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones
+{
+    public class Solution
+    {
+        public bool CheckOnesSegment(string s)
+        {
+            return !s.Contains("01");
+        }
+    }
+}
