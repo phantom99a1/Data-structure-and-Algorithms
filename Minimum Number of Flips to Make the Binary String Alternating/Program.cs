32231551dc7c09b3726d08e8267af8e9b5d847d7
phantom99a1 <viendanho99a1@gmail.com>
2026-03-07 15:46:11 +0700

    Add the problem Minimum Number of Flips to Make the Binary String Alternating

---
+++ Minimum Number of Flips to Make the Binary String Alternating/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating
+{
+    public class Solution
+    {
+        public int MinFlips(string s)
+        {
+            int n = s.Length;
+            string t = s + s;
+
+            int mis0 = 0;
+            int ans = n;
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+
+                char expected = (i % 2 == 0) ? '0' : '1';
+
+                if (t[i] != expected) mis0++;
+
+                if (i >= n)
+                {
+                    int left = i - n;
+                    char expLeft = (left % 2 == 0) ? '0' : '1';
+                    if (t[left] != expLeft) mis0--;
+                }
+
+                if (i >= n - 1)
+                {
+                    int mis1 = n - mis0;
+                    ans = Math.Min(ans, Math.Min(mis0, mis1));
+                }
+            }
+
+            return ans;
+        }
+    }
+}
