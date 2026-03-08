4dac91a8cad2ea4f4ba4423d2917382fd0d7f750
phantom99a1 <viendanho99a1@gmail.com>
2026-03-08 11:15:46 +0700

    Add the problem Find Kth Bit in Nth Binary String

---
+++ Find Kth Bit in Nth Binary String/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Find_Kth_Bit_in_Nth_Binary_String
+{
+    public class Solution
+    {
+        public char FindKthBit(int n, int k)
+        {
+            if (n == 1) return '0';
+
+            int len = (1 << n) - 1;
+            int mid = (len + 1) / 2;
+
+            if (k == mid) return '1';
+            if (k < mid) return FindKthBit(n - 1, k);
+
+            char c = FindKthBit(n - 1, len - k + 1);
+            return c == '0' ? '1' : '0';
+        }
+    }
+}
