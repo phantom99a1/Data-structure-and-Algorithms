784591764cd8623eba37960d411c4c516306882f
phantom99a1 <viendanho99a1@gmail.com>
2026-03-08 11:14:17 +0700

    Add the problem Find Unique Binary String

---
+++ Find Unique Binary String version 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Find_Unique_Binary_String_version_2
+{
+    public class Solution
+    {
+        public string FindDifferentBinaryString(string[] nums)
+        {
+            return string.Create(nums.Length, nums, static (span, arr) => {
+                for (int i = 0; i < span.Length; i++)
+                {
+                    span[i] = (char)(arr[i][i] ^ 1);
+                }
+            });
+        }
+    }
+}
