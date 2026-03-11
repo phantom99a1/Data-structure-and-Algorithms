6c95b74935a933ada3e1bf2b96a6d46016fbb97c
phantom99a1 <viendanho99a1@gmail.com>
2026-03-11 21:20:25 +0700

    Add the problem Complement of Base 10 Integer

---
+++ Complement of Base 10 Integer/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Complement_of_Base_10_Integer
+{
+    public class Solution
+    {
+        public int BitwiseComplement(int n)
+        {
+
+            if (n == 0) return 1;
+
+            int bits = (int)Math.Log(n, 2) + 1;
+            int mask = (1 << bits) - 1;
+
+            return n ^ mask;
+        }
+    }
+}
