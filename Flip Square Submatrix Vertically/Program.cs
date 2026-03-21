0307a68d42f6c590f66b15014a87d8875e3142a2
phantom99a1 <viendanho99a1@gmail.com>
2026-03-21 11:05:01 +0700

    Add the problem Flip Square Submatrix Vertically

---
+++ Flip Square Submatrix Vertically/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Flip_Square_Submatrix_Vertically
+{
+    public class Solution
+    {
+        public int[][] ReverseSubmatrix(int[][] grid, int x, int y, int k)
+        {
+            for (int i0 = x, i1 = x + k - 1; i0 < i1; i0++, i1--)
+            {
+                for (int j = y; j < y + k; j++)
+                {
+                    int temp = grid[i0][j];
+                    grid[i0][j] = grid[i1][j];
+                    grid[i1][j] = temp;
+                }
+            }
+            return grid;
+        }
+    }
+}
