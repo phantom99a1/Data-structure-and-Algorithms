685720ca2f7e30a47b0a39e329e7f2b10066d6fc
phantom99a1 <viendanho99a1@gmail.com>
2026-03-27 18:33:21 +0700

    Add the problem Matrix Similarity After Cyclic Shifts

---
+++ Matrix Similarity After Cyclic Shifts/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Matrix_Similarity_After_Cyclic_Shifts
+{
+    public class Solution
+    {
+        public bool AreSimilar(int[][] mat, int k)
+        {
+            int m = mat.Length;
+            int n = mat[0].Length;
+            k %= n;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i][j] != mat[i][(j + k) % n])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
