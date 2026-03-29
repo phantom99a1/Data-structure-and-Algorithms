22d2f1c4a728a4478299f86214ad6876eb8a8c40
phantom99a1 <viendanho99a1@gmail.com>
2026-03-29 09:59:19 +0700

    Add the problem Check if Strings Can be Made Equal With Operations I

---
+++ Check if Strings Can be Made Equal With Operations I/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Check_if_Strings_Can_be_Made_Equal_With_Operations_I
+{
+    public class Solution
+    {
+        public bool CanBeEqual(string s1, string s2)
+        {
+            return ((s1[0] == s2[0] && s1[2] == s2[2]) ||
+                    (s1[0] == s2[2] && s1[2] == s2[0])) &&
+                   ((s1[1] == s2[1] && s1[3] == s2[3]) ||
+                    (s1[1] == s2[3] && s1[3] == s2[1]));
+        }
+    }
+}
