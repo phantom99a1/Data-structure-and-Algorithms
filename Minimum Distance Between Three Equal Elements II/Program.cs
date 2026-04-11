fb865862791b1a754215caf28a037d58a22d9d9f
phantom99a1 <viendanho99a1@gmail.com>
2026-04-11 11:35:04 +0700

    Add the problem Minimum Distance Between Three Equal Elements II

---
+++ Minimum Distance Between Three Equal Elements II/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Minimum_Distance_Between_Three_Equal_Elements_II
+{
+    public class Solution
+    {
+        public int MinimumDistance(int[] nums)
+        {
+            var count = 3;
+            var map = new Dictionary<int, LinkedList<int>>();
+            var result = int.MaxValue;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (!map.TryGetValue(nums[i], out var list))
+                {
+                    var newList = new LinkedList<int>();
+                    newList.AddLast(i);
+                    map.Add(nums[i], newList);
+                }
+                else
+                {
+                    list.AddLast(i);
+
+                    if (list.Count > count)
+                        list.RemoveFirst();
+
+                    if (list.Count == count)
+                    {
+                        var dist = (list.Last.Value - list.First.Value) * 2;
+                        result = Math.Min(result, dist);
+                    }
+                }
+            }
+
+            return result == int.MaxValue ? -1 : result;
+        }
+    }
+}
