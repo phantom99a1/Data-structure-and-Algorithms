2c3b3d39bf5549b2b2ef29a5c45350e5ca474189
phantom99a1 <viendanho99a1@gmail.com>
2026-04-13 20:37:57 +0700

    Add the problem Minimum Distance to the Target Element

---
+++ Minimum Distance to the Target Element/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Minimum_Distance_to_the_Target_Element
+{
+    public class Solution
+    {
+        public int GetMinDistance(int[] nums, int target, int start) => nums.
+            Select((m, i) => new { value = m, index = i }).
+            Where(m => m.value == target).
+            Select((m, i) => Math.Abs(m.index - start)).
+            Min();
+    }
+}
