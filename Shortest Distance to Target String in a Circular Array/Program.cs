c722f879ee22af0e188aea38d02802063059d3c1
phantom99a1 <viendanho99a1@gmail.com>
2026-04-16 00:18:51 +0700

    Add the problem Shortest Distance to Target String in a Circular Array

---
+++ Shortest Distance to Target String in a Circular Array/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Shortest_Distance_to_Target_String_in_a_Circular_Array
+{
+    public class Solution
+    {
+        public int ClosestTarget(string[] words, string target, int startIndex) => words.Contains(target)
+            ? words.Select((m, i) => (m, i)).
+                Where(m => m.m == target).
+                Min(m => Math.Min(Math.Abs(m.i - startIndex), Math.Abs(words.Length - Math.Abs(m.i - startIndex))))
+            : -1;
+    }
+}
