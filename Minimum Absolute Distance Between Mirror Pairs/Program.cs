d39240169c12122963dee6560495374c713bc9b0
phantom99a1 <viendanho99a1@gmail.com>
2026-04-17 23:13:30 +0700

    Add the problem Minimum Absolute Distance Between Mirror Pairs

---
+++ Minimum Absolute Distance Between Mirror Pairs/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Minimum_Absolute_Distance_Between_Mirror_Pairs
+{
+    public class Solution
+    {
+        public int MinMirrorPairDistance(int[] nums)
+        {
+            Dictionary<int, int> prev = new Dictionary<int, int>();
+            int n = nums.Length;
+            int ans = n + 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = nums[i];
+                if (prev.ContainsKey(x))
+                {
+                    ans = Math.Min(ans, i - prev[x]);
+                }
+                prev[ReverseNum(x)] = i;
+            }
+
+            return ans == n + 1 ? -1 : ans;
+        }
+
+        private int ReverseNum(int x)
+        {
+            int y = 0;
+            while (x > 0)
+            {
+                y = y * 10 + x % 10;
+                x /= 10;
+            }
+            return y;
+        }
+    }
+}
