8c401b05860e1a0928d6c6bcc8c57c8d139eb603
phantom99a1 <viendanho99a1@gmail.com>
2026-04-19 09:54:52 +0700

    Add the problem Maximum Distance Between a Pair of Values

---
+++ Maximum Distance Between a Pair of Values/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Maximum_Distance_Between_a_Pair_of_Values
+{
+    public class Solution
+    {
+        public int MaxDistance(int[] nums1, int[] nums2)
+        {
+            int i = 0, j = 0, ans = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    ans = Math.Max(ans, j - i);
+                    j++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
