38d738864401d6a14e689eca14310569ae09e2e3
phantom99a1 <viendanho99a1@gmail.com>
2026-04-19 10:08:11 +0700

    Add the problem Mirror Distance of an Integer

---
+++ Mirror Distance of an Integer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Mirror_Distance_of_an_Integer
+{
+    public class Solution
+    {
+        public int Reverse(int n)
+        {
+            int res = 0;
+            while (n > 0)
+            {
+                res = res * 10 + n % 10;
+                n /= 10;
+            }
+            return res;
+        }
+
+        public int MirrorDistance(int n)
+        {
+            return Math.Abs(n - Reverse(n));
+        }
+    }
+}
