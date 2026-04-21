6a4a155474be85c945687144faf4d25a7ecf077d
phantom99a1 <viendanho99a1@gmail.com>
2026-04-21 18:39:08 +0700

    Add the problem Two Furthest Houses With Different Colors

---
+++ Two Furthest Houses With Different Colors/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int MaxDistance(int[] colors)
+    {
+        int dis = 0;
+        int n = colors.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (colors[i] != colors[j])
+                {
+                    dis = Math.Max(dis, j - i);
+                }
+            }
+        }
+
+        return dis;
+    }
+}
