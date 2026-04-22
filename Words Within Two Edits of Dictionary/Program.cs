51a82bbbde96563e3899349a37fac837cff8d31a
phantom99a1 <viendanho99a1@gmail.com>
2026-04-22 12:40:20 +0700

    Add the problem Words Within Two Edits of Dictionary

---
+++ Words Within Two Edits of Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words_Within_Two_Edits_of_Dictionary
+{
+    public class Solution
+    {
+        public IList<string> TwoEditWords(string[] queries, string[] dictionary)
+        {
+            List<string> ans = new List<string>();
+
+            foreach (string q in queries)
+            {
+                foreach (string d in dictionary)
+                {
+                    int diff = 0;
+
+                    for (int i = 0; i < q.Length; i++)
+                    {
+                        if (q[i] != d[i])
+                        {
+                            diff++;
+                        }
+
+                        if (diff > 2) break;
+                    }
+
+                    if (diff <= 2)
+                    {
+                        ans.Add(q);
+                        break;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Words Within Two Edits of Dictionary/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Words Within Two Edits of Dictionary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Words Within Two Edits of Dictionary")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f176ee34-f5ba-46de-b879-401e67f0c49a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
