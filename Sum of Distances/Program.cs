fc29bf19dd0eeca13c7d00381eb3e874d5fcf6d7
phantom99a1 <viendanho99a1@gmail.com>
2026-04-23 23:20:31 +0700

    Add the problem Sum of Distances

---
+++ Sum of Distances/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Distances
+{
+    public class Solution
+    {
+        public long[] Distance(int[] nums)
+        {
+            int n = nums.Length;
+            var groups = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                if (!groups.ContainsKey(nums[i]))
+                {
+                    groups[nums[i]] = new List<int>();
+                }
+                groups[nums[i]].Add(i);
+            }
+            long[] res = new long[n];
+            foreach (var group in groups.Values)
+            {
+                long total = 0;
+                foreach (int idx in group)
+                {
+                    total += idx;
+                }
+                long prefixTotal = 0;
+                int sz = group.Count;
+                for (int i = 0; i < sz; i++)
+                {
+                    int idx = group[i];
+                    res[idx] = total - prefixTotal * 2 + (long)idx * (2 * i - sz);
+                    prefixTotal += idx;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Sum of Distances/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sum of Distances")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sum of Distances")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb66a57b-25c5-47e3-b2f5-48898bc888c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
