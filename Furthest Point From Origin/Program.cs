b2a77971a5083b7b685553329ab4e35dd153cb5e
phantom99a1 <viendanho99a1@gmail.com>
2026-04-24 22:41:00 +0700

    Add the problem Furthest Point From Origin

---
+++ Furthest Point From Origin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furthest_Point_From_Origin
+{
+    public class Solution
+    {
+        public int FurthestDistanceFromOrigin(string moves)
+        {
+            int L = 0, R = 0, B = 0;
+            foreach (char c in moves)
+            {
+                if (c == 'L')
+                {
+                    L++;
+                }
+                else if (c == 'R')
+                {
+                    R++;
+                }
+                else
+                {
+                    B++;
+                }
+            }
+            return Math.Abs(L - R) + B;
+        }
+    }
+}
