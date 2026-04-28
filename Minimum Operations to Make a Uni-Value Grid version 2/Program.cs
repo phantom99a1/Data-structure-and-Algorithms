2dd4df2f81cb054d9e65f98e0005bfd7bae5c0f9
phantom99a1 <viendanho99a1@gmail.com>
2026-04-28 22:11:55 +0700

    Add the problem Minimum Operations to Make a Uni-Value Grid

---
+++ Minimum Operations to Make a Uni-Value Grid version 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Operations_to_Make_a_Uni_Value_Grid_version_2
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public int MinOperations(int[][] grid, int x)
+        {
+            List<int> arr = new List<int>();
+            foreach (var row in grid)
+            {
+                foreach (var num in row)
+                {
+                    arr.Add(num);
+                }
+            }
+
+            arr.Sort();
+            int median = arr[arr.Count / 2];
+            int operations = 0;
+
+            foreach (int num in arr)
+            {
+                int diff = Math.Abs(num - median);
+                if (diff % x != 0) return -1;
+                operations += diff / x;
+            }
+            return operations;
+        }
+    }
+}
