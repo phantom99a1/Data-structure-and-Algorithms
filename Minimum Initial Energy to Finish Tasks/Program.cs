c780e509b2baa00e5e420d3afdaa2bc8c1288d18
phantom99a1 <viendanho99a1@gmail.com>
2026-05-12 18:29:10 +0700

    Add the problem Minimum Initial Energy to Finish Tasks

---
+++ Minimum Initial Energy to Finish Tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Minimum_Initial_Energy_to_Finish_Tasks
+{
+    public class Solution
+    {
+
+        public int MinimumEffort(int[][] tasks)
+        {
+
+            tasks.ToList().Sort((x, y) => {
+
+                var a = x[1] - x[0];
+                var b = y[1] - y[0];
+
+                return a.CompareTo(b);
+            });
+
+            int result = 0;
+
+            foreach (var task in tasks)
+            {
+
+                result = Math.Max(result + task[0], task[1]);
+            }
+
+            return result;
+        }
+    }
+}
