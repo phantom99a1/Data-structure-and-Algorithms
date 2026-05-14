672d493ac944b7b1f874697cf923335359015956
phantom99a1 <viendanho99a1@gmail.com>
2026-05-14 22:23:45 +0700

    Add the problem Check if Array is Good

---
+++ Check if Array is Good/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Check_if_Array_is_Good
+{
+    public class Solution
+    {
+        public bool IsGood(int[] nums)
+        {
+            int n = nums.Length;
+            int[] count = new int[n];
+            foreach (int a in nums)
+            {
+                if (a < 1 || a >= n)
+                {
+                    return false;
+                }
+                if (a < n - 1 && count[a] > 0)
+                {
+                    return false;
+                }
+                if (a == n - 1 && count[a] > 1)
+                {
+                    return false;
+                }
+                count[a]++;
+            }
+            return true;
+        }
+    }
+}
