c29ed88d841932455285d5196a2ea86db8e409e9
phantom99a1 <viendanho99a1@gmail.com>
2026-05-15 23:16:44 +0700

    Add the problem Find Minimum in Rotated Sorted Array

---
+++ Find Minimum in Rotated Sorted Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_Minimum_in_Rotated_Sorted_Array
+{
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            int left = 0, right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (nums[left] < nums[right])
+                    return nums[left];
+
+                int mid = left + (right - left) / 2;
+
+                if (nums[right] > nums[mid]) right = mid;
+                else left = mid + 1;
+            }
+
+            return nums[left];
+        }
+    }
+}
