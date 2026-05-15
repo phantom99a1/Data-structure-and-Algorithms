65fdb3951f64cd5ff1c3e372a1a7a52767fa57ae
phantom99a1 <viendanho99a1@gmail.com>
2026-05-15 23:21:03 +0700

    Add the problem Separate the Digits in an Array

---
+++ Separate the Digits in an Array/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Separate_the_Digits_in_an_Array
+{
+    public class Solution
+    {
+        public int[] SeparateDigits(int[] nums)
+        {
+            return (string.Join(null, nums).ToCharArray()).Select(item => item - '0').ToArray();
+        }
+    }
+}
