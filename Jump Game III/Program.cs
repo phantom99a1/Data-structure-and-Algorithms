35837dbce98f1c3178c71d96d0829bcf5e733705
phantom99a1 <viendanho99a1@gmail.com>
2026-05-17 21:53:26 +0700

    Add the problem Jump Game III

---
+++ Jump Game III/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Jump_Game_III
+{
+    public class Solution
+    {
+        public bool CanReach(int[] arr, int start)
+        {
+            int n = arr.Length;
+
+            bool[] visited = new bool[n];
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(start);
+
+            while (q.Count > 0)
+            {
+                int i = q.Dequeue();
+
+                if (i < 0 || i >= n || visited[i])
+                    continue;
+
+                if (arr[i] == 0)
+                    return true;
+
+                visited[i] = true;
+
+                q.Enqueue(i + arr[i]);
+                q.Enqueue(i - arr[i]);
+            }
+
+            return false;
+        }
+    }
+}
