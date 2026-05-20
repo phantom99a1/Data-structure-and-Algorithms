f288d4d91cfc909b676aa296ea5e5d40189870c3
phantom99a1 <viendanho99a1@gmail.com>
2026-05-21 00:12:42 +0700

    Add the problem Minimum Common Value

---
+++ Minimum Common Value/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Minimum_Common_Value
+{
+    public class Solution
+    {
+        public int GetCommon(int[] nums1, int[] nums2) => nums1
+            .Intersect(nums2)
+            .FirstOrDefault(-1);
+    }
+}
