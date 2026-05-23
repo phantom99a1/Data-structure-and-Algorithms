dbe85c9cd7353cb6e03f80ccc2b301515eb05684
phantom99a1 <viendanho99a1@gmail.com>
2026-05-23 17:15:33 +0700

    Add the problem Check if Array Is Sorted and Rotated version 2

---
+++ Check if Array Is Sorted and Rotated version 2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Check_if_Array_Is_Sorted_and_Rotated_version_2
+{
+    public class Solution
+    {
+        public bool Check(int[] nums)
+        {
+            int count = 0;  // To count the number of drops
+            int n = nums.Length;
+
+            // Check the array for drops
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    count++;
+                }
+            }
+
+            // Check the wrap-around condition
+            if (nums[n - 1] > nums[0])
+            {
+                count++;
+            }
+
+            // If there is more than one drop, it is not a rotated sorted array
+            if (count > 1)
+            {
+                return false;
+            }
+
+            // Otherwise, it is a rotated sorted array
+            return true;
+        }
+    }
+}
