868ae666b749b4729e510c5561ed622b2c063a18
phantom99a1 <viendanho99a1@gmail.com>
2026-05-26 20:02:16 +0700

    Add the problem Count the Number of Special Characters I

---
+++ Count the Number of Special Characters I/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Count_the_Number_of_Special_Characters_I
+{
+    public class Solution
+    {
+        public int NumberOfSpecialChars(string word) => word.
+            ToLower().
+            GroupBy(m => m).
+            Count(m => word.Contains(char.ToLower(m.Key)) && word.Contains(char.ToUpper(m.Key)));
+    }
+}
