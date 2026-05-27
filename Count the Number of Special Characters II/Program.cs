dcd2ddaa7f75e3ba8bc3b06d84428b767b8b91af
phantom99a1 <viendanho99a1@gmail.com>
2026-05-27 20:34:53 +0700

    Add the problem Count the Number of Special Characters II

---
+++ Count the Number of Special Characters II/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Count_the_Number_of_Special_Characters_II
+{
+    using System.Collections.Generic;
+
+    public class Solution
+    {
+        public int NumberOfSpecialChars(string word)
+        {
+            int[] lastLower = new int[26];
+            int[] firstUpper = new int[26];
+
+            for (int i = 0; i < 26; i++)
+            {
+                lastLower[i] = -1;
+                firstUpper[i] = -1;
+            }
+
+            HashSet<int> invalid = new HashSet<int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    int idx = ch - 'a';
+
+                    lastLower[idx] = i;
+
+                    if (firstUpper[idx] != -1)
+                    {
+                        invalid.Add(idx);
+                    }
+
+                }
+                else
+                {
+                    int idx = ch - 'A';
+
+                    if (firstUpper[idx] == -1)
+                    {
+                        firstUpper[idx] = i;
+                    }
+                }
+            }
+
+            int specialCount = 0;
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (lastLower[i] != -1 &&
+                    firstUpper[i] != -1 &&
+                    !invalid.Contains(i))
+                {
+
+                    specialCount++;
+                }
+            }
+
+            return specialCount;
+        }
+    }
+}
