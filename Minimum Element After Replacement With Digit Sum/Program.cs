5c6bbfdd4b52808fb09c49e6f4968836b3228808
phantom99a1 <viendanho99a1@gmail.com>
2026-05-29 23:58:35 +0700

    Add the problem Minimum Element After Replacement With Digit Sum

---
+++ Minimum Element After Replacement With Digit Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Minimum_Element_After_Replacement_With_Digit_Sum
+{
+    public class Solution
+    {
+        public int MinElement(int[] nums) => nums.
+            Select(n => n.ToString().
+                    ToCharArray().
+                    Sum(n => (int)char.GetNumericValue(n))).
+            Min();
+    }
+}
