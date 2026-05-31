b53fc3f26557352c50aea1028c4e7dcf1a363357
phantom99a1 <viendanho99a1@gmail.com>
2026-05-31 13:39:42 +0700

    Add the problem Destroying Asteroids

---
+++ Destroying Asteroids/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Destroying_Asteroids
+{
+    public class Solution
+    {
+        public bool AsteroidsDestroyed(
+            int mass,
+            int[] asteroids
+        )
+        {
+
+            Array.Sort(asteroids);
+
+            long currentMass = mass;
+
+            foreach (int asteroid in asteroids)
+            {
+                if (currentMass < asteroid)
+                {
+                    return false;
+                }
+
+                currentMass += asteroid;
+            }
+
+            return true;
+        }
+    }
+}
