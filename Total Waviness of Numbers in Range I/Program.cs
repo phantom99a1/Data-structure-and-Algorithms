e5054680743bc3275f2f21e5f2ea4e5cc2671d8c
phantom99a1 <viendanho99a1@gmail.com>
2026-06-04 18:14:31 +0700

    Add the problem Total Waviness of Numbers in Range I

---
+++ Total Waviness of Numbers in Range I/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Total_Waviness_of_Numbers_in_Range_I
+{
+    public class Solution
+    {
+        public int TotalWaviness(int num1, int num2)
+        {
+            int result = 0;
+            var dp = new int[num2 + 1];
+
+            for (int num = 0; num <= num2 / 10; num++)
+                CountWaviness(num, dp);
+
+            for (int num = num1; num <= num2; num++)
+                result += CountWaviness(num, dp);
+
+            return result;
+        }
+
+        private int CountWaviness(int num, int[] dp)
+        {
+            if (num < 100) return 0;
+
+            int i = num % 10;
+            int j = (num / 10) % 10;
+            int k = (num / 100) % 10;
+
+            dp[num] = dp[num / 10] + (
+                (i < j && j > k) || (i > j && j < k) ? 1 : 0
+            );
+
+            return dp[num];
+        }
+    }
+}
