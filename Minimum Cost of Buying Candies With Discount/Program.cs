736dacc533e97f62f99f1d0a5c4af793c2a135bd
phantom99a1 <viendanho99a1@gmail.com>
2026-06-05 18:08:11 +0700

    Add the problem Minimum Cost of Buying Candies With Discount

---
+++ Minimum Cost of Buying Candies With Discount/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Minimum_Cost_of_Buying_Candies_With_Discount
+{
+    public class Solution
+    {
+        public int MinimumCost(int[] cost)
+        {
+            Array.Sort(cost, (a, b) => b.CompareTo(a));
+            (int i, int total) = (0, 0);
+            while (i < cost.Length)
+            {
+                total += cost[i];
+                if (i + 1 < cost.Length)
+                {
+                    total += cost[i + 1];
+                }
+
+                i += 3;
+            }
+
+            return total;
+        }
+    }
+}
