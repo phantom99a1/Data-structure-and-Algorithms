d52c44676f10d65e5a9073175d3bec528f510c02
phantom99a1 <viendanho99a1@gmail.com>
2026-06-06 08:34:25 +0700

    Add the problem Left and Right Sum Differences

---
+++ Left and Right Sum Differences/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Left_and_Right_Sum_Differences
+{
+    public class Solution
+    {
+        public int[] LeftRightDifference(int[] nums)
+        {
+            int n = nums.Length;
+            int[] leftSum = new int[n];
+            int[] rightSum = new int[n];
+            int[] newSum = new int[n];
+
+            for (int i = 1; i < n; i++)
+            {
+                leftSum[i] = leftSum[i - 1] + nums[i - 1];
+            }
+
+            for (int j = n - 2; j >= 0; j--)
+            {
+                rightSum[j] = rightSum[j + 1] + nums[j + 1];
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                newSum[k] = Math.Abs(leftSum[k] - rightSum[k]);
+            }
+
+            return newSum;
+        }
+    }
+}
