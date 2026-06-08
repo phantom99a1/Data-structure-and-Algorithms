90e65647a962094d20db2254e43b5cb67b45be12
phantom99a1 <viendanho99a1@gmail.com>
2026-06-08 18:18:35 +0700

    Add the problem Partition Array According to Given Pivot version 2

---
+++ Partition Array According to Given Pivot version 2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Partition_Array_According_to_Given_Pivot_version_2
+{
+    public class Solution
+    {
+        public int[] PivotArray(int[] nums, int pivot)
+        {
+            List<int> left = new();
+            List<int> same = new();
+            List<int> right = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < pivot)
+                {
+                    left.Add(nums[i]);
+                }
+                else if (nums[i] > pivot)
+                {
+                    right.Add(nums[i]);
+                }
+                else
+                {
+                    same.Add(nums[i]);
+                }
+            }
+
+            left.AddRange(same);
+            left.AddRange(right);
+
+            return [.. left];
+        }
+    }
+}
