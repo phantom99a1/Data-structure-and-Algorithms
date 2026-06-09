5463a0f96747b193b8faa736995ddea41b41188a
phantom99a1 <viendanho99a1@gmail.com>
2026-06-09 21:08:58 +0700

    Add the problem Maximum Total Subarray Value I

---
+++ Maximum Total Subarray Value I/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Maximum_Total_Subarray_Value_I
+{
+    public class Solution
+    {
+        public long MaxTotalValue(int[] A, int k)
+        {
+            int gMin = A[0], gMax = A[0];
+
+            foreach (int n in A)
+            {
+                gMin = Math.Min(gMin, n);
+                gMax = Math.Max(gMax, n);
+            }
+
+            return (long)(gMax - gMin) * k;
+        }
+    }
+}
