94c76ed04989d3287211633b9c7d242c31d4ee57
phantom99a1 <viendanho99a1@gmail.com>
2026-06-13 13:44:20 +0700

    Add the prolem Weighted Word Mapping

---
+++ Weighted Word Mapping/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Weighted_Word_Mapping
+{
+    public class Solution
+    {
+        public string MapWordWeights(string[] words, int[] weights)
+        {
+            var ans = new StringBuilder(words.Length);
+            foreach (string word in words)
+            {
+                int s = 0;
+                foreach (char c in word)
+                {
+                    s += weights[c - 'a'];
+                }
+                ans.Append((char)('z' - s % 26));
+            }
+            return ans.ToString();
+        }
+    }
+}
