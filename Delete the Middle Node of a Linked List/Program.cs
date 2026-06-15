e8d6b9cf03d19905003c7e17bb14ae834c039e48
phantom99a1 <viendanho99a1@gmail.com>
2026-06-15 18:09:54 +0700

    Add the problem Delete the Middle Node of a Linked List

---
+++ Delete the Middle Node of a Linked List/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Delete_the_Middle_Node_of_a_Linked_List
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            if (head.next == null) { return head.next; }
+            ListNode f = head, d = head;
+            while (d.next != null && d.next.next != null && d.next.next.next != null)
+            {
+                d = d.next.next;
+                f = f.next;
+            }
+            f.next = f.next.next;
+            return head;
+        }
+    }
+}
