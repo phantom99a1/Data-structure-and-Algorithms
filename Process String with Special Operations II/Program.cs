c633be1034d86c9c7538bcb375a2649ede694212
phantom99a1 <viendanho99a1@gmail.com>
2026-06-17 18:28:56 +0700

    Add the problem Process String with Special Operations II

---
+++ Process String with Special Operations II/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Process_String_with_Special_Operations_II
+{
+    public class Solution
+    {
+        public char ProcessStr(string s, long k)
+        {
+            long len = 0;
+
+            foreach (char ch in s)
+            {
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    len++;
+                }
+                else if (ch == '*')
+                {
+                    if (len > 0) len--;
+                }
+                else if (ch == '#')
+                {
+                    len *= 2;
+                }
+            }
+
+            if (k >= len) return '.';
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                char ch = s[i];
+
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    if (k == len - 1) return ch;
+                    len--;
+                }
+                else if (ch == '*')
+                {
+                    len++;
+                }
+                else if (ch == '#')
+                {
+                    long prevLen = len / 2;
+                    k %= prevLen;
+                    len = prevLen;
+                }
+                else
+                {
+                    k = len - 1 - k;
+                }
+            }
+
+            return '.';
+        }
+    }
+}
