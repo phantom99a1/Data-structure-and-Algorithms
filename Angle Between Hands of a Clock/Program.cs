45e9a181c8769fbea802c4c5f6cb780a51c33eeb
phantom99a1 <viendanho99a1@gmail.com>
2026-06-18 18:55:13 +0700

    Add the problem Angle Between Hands of a Clock

---
+++ Angle Between Hands of a Clock/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Angle_Between_Hands_of_a_Clock
+{
+    public class Solution
+    {
+        public double AngleClock(int hour, int minutes)
+        {
+            hour %= 12;
+            double minAngle = 6.0 * minutes;
+            double hourAngle = 30.0 * hour + 0.5 * minutes;
+            double diff = Math.Abs(hourAngle - minAngle);
+
+            return Math.Min(diff, 360.0 - diff);
+        }
+    }
+}
