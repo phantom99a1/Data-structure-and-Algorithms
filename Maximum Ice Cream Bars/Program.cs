a2a664c9d0f066dc9cc23cc41f20d52cd6687fcb
phantom99a1 <viendanho99a1@gmail.com>
2026-06-21 09:20:24 +0700

    Add the problem Maximum Ice Cream Bars

---
+++ Maximum Ice Cream Bars/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Maximum_Ice_Cream_Bars
+{
+    public class Solution
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            int count = 0;
+            int sum = 0;
+            Array.Sort(costs);
+            for (int i = 0; i < costs.Length; i++)
+            {
+                if (sum + costs[i] <= coins)
+                {
+                    sum += costs[i];
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
