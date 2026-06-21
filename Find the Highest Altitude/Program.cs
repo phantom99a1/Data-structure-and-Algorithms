c3538c582a36b1ca1b7a83a2684c893c6c30ab4c
phantom99a1 <viendanho99a1@gmail.com>
2026-06-21 09:29:53 +0700

    Add the problem Find the Highest Altitude

---
+++ Find the Highest Altitude/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Find_the_Highest_Altitude
+{
+    public class Solution
+    {
+        public int LargestAltitude(int[] gain) => gain.
+            Aggregate((0, 0), (acum, current) => (
+                acum.Item1 + current,
+                Math.Max(acum.Item2, acum.Item1 + current)
+            ), result => result.Item2);
+    }
+}
