807cc80289268399278c1f0e5b405866b14f46f8
phantom99a1 <viendanho99a1@gmail.com>
2026-06-25 18:45:53 +0700

    Add the problem Count Subarrays With Majority Element I

---
+++ Count Subarrays With Majority Element I/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Count_Subarrays_With_Majority_Element_I
+{
+    public class Solution
+    {
+        public int CountMajoritySubarrays(int[] nums, int target)
+        {
+            int n = nums.Length;
+            int ans = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                int cnt = 0;
+                for (int j = i; j < n; ++j)
+                {
+                    cnt += (nums[j] == target ? 1 : -1);
+                    if (cnt > 0)
+                    {
+                        ++ans;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
