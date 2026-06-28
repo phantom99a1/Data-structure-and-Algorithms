f8cb4a69bfa67cdc47c7ec0f85a408ff7ea2256e
phantom99a1 <viendanho99a1@gmail.com>
2026-06-28 09:30:41 +0700

    Add the problem Maximum Element After Decreasing and Rearranging

---
+++ Maximum Element After Decreasing and Rearranging/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Maximum_Element_After_Decreasing_and_Rearranging
+{
+    public class Solution
+    {
+        public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+        {
+            int n = arr.Length;
+            Array.Sort(arr);
+            int prev = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] >= prev + 1) prev += 1;
+            }
+
+            return prev;
+        }
+    }
+}
