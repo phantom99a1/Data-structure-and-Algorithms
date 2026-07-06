ac348c99699c1d054ac900b5e81b1539adb338b1
phantom99a1 <viendanho99a1@gmail.com>
2026-07-06 21:46:15 +0700

    Add the problem Remove Covered Intervals

---
+++ Remove Covered Intervals/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Remove_Covered_Intervals
+{
+    public class Solution
+    {
+        public int RemoveCoveredIntervals(int[][] intervals)
+        {
+            Array.Sort(intervals, (a, b) =>
+            {
+                var comp = a[0].CompareTo(b[0]);
+                if (comp == 0)
+                    comp = b[1].CompareTo(a[1]);
+                return comp;
+            });
+
+            var count = 0;
+            int maxEnd = 0;
+            foreach (int[] curr in intervals)
+            {
+                if (maxEnd < curr[1])
+                {
+                    count++;
+                    maxEnd = curr[1];
+                }
+            }
+
+            return count;
+        }
+    }
+}
