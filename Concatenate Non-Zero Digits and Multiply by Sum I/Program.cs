3e0f4bc4b19f04b8948e301adab39f1bff8283c0
phantom99a1 <viendanho99a1@gmail.com>
2026-07-07 18:29:19 +0700

    Add the problem Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ Concatenate Non-Zero Digits and Multiply by Sum I/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Concatenate_Non_Zero_Digits_and_Multiply_by_Sum_I
+{
+    public class Solution
+    {
+        public long SumAndMultiply(int n)
+        {
+            long x = 0, sum = 0, pow10 = 1;
+            while (n > 0)
+            {
+                int d = n % 10;
+                sum += d;
+                if (d > 0)
+                {
+                    x += d * pow10;
+                    pow10 *= 10;
+                }
+                n /= 10;
+            }
+            return x * sum;
+        }
+    }
+}
