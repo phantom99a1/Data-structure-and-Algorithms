c0ccbf7ef7aa55d43fb52c6e46ad85cdac5fd7bb
phantom99a1 <viendanho99a1@gmail.com>
2026-07-12 09:01:43 +0700

    Add the problem Rank Transform of an Array

---
+++ Rank Transform of an Array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Rank_Transform_of_an_Array
+{
+    public class Solution
+    {
+        public int[] ArrayRankTransform(int[] arr) => arr
+            .Select((num, i) => (num, index: i))
+            .GroupBy(t => t.num, t => t.index)
+            .OrderBy(gr => gr.Key)
+            .Select((gr, i) => (rank: i + 1, indexes: gr))
+            .SelectMany(p => p.indexes, (p, index) => (p.rank, index))
+            .OrderBy(r => r.index)
+            .Select(r => r.rank)
+            .ToArray();
+    }
+}
