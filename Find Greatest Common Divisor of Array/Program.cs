65d821d45490d8344ce751a1fce2bb85dc0a07e4
phantom99a1 <viendanho99a1@gmail.com>
2026-07-18 15:23:18 +0700

    Add the problem Find Greatest Common Divisor of Array

---
+++ Find Greatest Common Divisor of Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_Greatest_Common_Divisor_of_Array
+{
+    public class Solution
+    {
+        public int FindGCD(int[] nums)
+        {
+            int mx = nums.Max();
+            int mn = nums.Min();
+            return Gcd(mx, mn);
+        }
+
+        private int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+}
