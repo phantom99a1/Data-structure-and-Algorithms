fd80534ea9479d74696cf3dd9a53850b9dd7d4ce
phantom99a1 <viendanho99a1@gmail.com>
2026-07-21 22:21:13 +0700

    Add the problem Maximize Active Section with Trade I

---
+++ Maximize Active Section with Trade I/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Maximize_Active_Section_with_Trade_I
+{
+    public class Solution
+    {
+        public int MaxActiveSectionsAfterTrade(string s)
+        {
+            int n = s.Length;
+            int cnt1 = 0;
+            foreach (char c in s)
+            {
+                if (c == '1')
+                    cnt1++;
+            }
+
+            int i = 0;
+            int bestGain = 0;
+            int prev = int.MinValue;
+            int cur = 0;
+            while (i < n)
+            {
+                int start = i;
+                while (i < n && s[i] == s[start])
+                {
+                    i++;
+                }
+                if (s[start] == '0')
+                {
+                    cur = i - start;
+                    if (prev != int.MinValue)
+                    {
+                        bestGain = Math.Max(bestGain, prev + cur);
+                    }
+                    prev = cur;
+                }
+            }
+
+            return cnt1 + bestGain;
+        }
+    }
+}
