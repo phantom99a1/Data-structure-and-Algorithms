af8919eaa9217c68cc1eacfca26b2fc1f97be2ee
phantom99a1 <viendanho99a1@gmail.com>
2026-07-25 13:41:25 +0700

    Add the problem Number of Unique XOR Triplets I

---
+++ Number of Unique XOR Triplets I/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Number_of_Unique_XOR_Triplets_I
+{
+    public class Solution
+    {
+        public int UniqueXorTriplets(int[] nums)
+        {
+            int n = nums.Length;
+            if (n <= 2)
+            {
+                return n;
+            }
+            int ans = 1;
+            while (ans <= n)
+            {
+                ans <<= 1;
+            }
+            return ans;
+        }
+    }
+}
