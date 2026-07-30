8853d56051bb9a3d620bb463c5df166fb8a2f4ac
phantom99a1 <viendanho99a1@gmail.com>
2026-07-30 18:57:22 +0700

    Add the problem Minimum Number of Pushes to Type Word I

---
+++ Minimum Number of Pushes to Type Word I/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Minimum_Number_of_Pushes_to_Type_Word_I
+{
+    public class Solution
+    {
+        public int MinimumPushes(string word)
+        {
+            int n = word.Length;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                ans += i / 8 + 1;
+            }
+            return ans;
+        }
+    }
+}
