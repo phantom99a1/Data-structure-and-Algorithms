5777617fc8ba0c3e9eef0f68c88e742e2abfd6d6
phantom99a1 <viendanho99a1@gmail.com>
2026-08-01 14:42:09 +0700

    Add the problem Minimum Number of Pushes to Type Word II

---
+++ Minimum Number of Pushes to Type Word II/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Minimum_Number_of_Pushes_to_Type_Word_II
+{
+    public class Solution
+    {
+        public int MinimumPushes(string word)
+        {
+            int[] freq = new int[26];
+            foreach (char c in word)
+            {
+                freq[c - 'a']++;
+            }
+
+            Array.Sort(freq, (a, b) => b.CompareTo(a));
+
+            int count = 0;
+            int sum = 0;
+            foreach (int frequency in freq)
+            {
+                int presses = (count++ / 8) + 1;
+                sum += presses * frequency;
+            }
+
+            return sum;
+        }
+    }
+}
