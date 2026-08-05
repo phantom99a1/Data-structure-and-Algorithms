eca7a84d109d2117e610d215ad8401afae7fb78f
phantom99a1 <viendanho99a1@gmail.com>
2026-08-05 19:30:14 +0700

    Add the problem Find Missing Elements

---
+++ Find Missing Elements/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Find_Missing_Elements
+{
+    public class Solution
+    {
+        public IList<int> FindMissingElements(int[] nums)
+        {
+            var st = new HashSet<int>(nums);
+            int mn = nums.Min();
+            int mx = nums.Max();
+
+            var ans = new List<int>();
+            for (int i = mn + 1; i < mx; i++)
+            {
+                if (!st.Contains(i))
+                {
+                    ans.Add(i);
+                }
+            }
+            return ans;
+        }
+    }
+}
