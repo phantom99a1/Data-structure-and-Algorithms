3ccf53161ea3134b14282e2336d1f318d3315af7
phantom99a1 <viendanho99a1@gmail.com>
2026-08-06 19:19:44 +0700

    Add the problem Smallest Divisible Digit Product I

---
+++ Smallest Divisible Digit Product I/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Smallest_Divisible_Digit_Product_I
+{
+    public class Solution
+    {
+        public int SmallestNumber(int n, int t)
+        {
+            int product = 1;
+            int nCounter = n;
+            int counter = 0;
+            int result = n;
+
+            while (product % t != 0)
+            {
+                nCounter = n + counter;
+                product = 1;
+                result = nCounter;
+
+                while (nCounter != 0)
+                {
+                    int remainder = nCounter % 10;
+                    product = product * remainder;
+                    nCounter = nCounter / 10;
+                }
+                counter++;
+            }
+            return result;
+        }
+    }
+}
