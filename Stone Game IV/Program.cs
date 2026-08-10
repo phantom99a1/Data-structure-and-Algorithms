94ab022939aae6f32cbfd2df8a3ce41cc36d72b3
phantom99a1 <viendanho99a1@gmail.com>
2026-08-10 22:10:11 +0700

    Add the problem Stone Game IV

---
+++ Stone Game IV/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Stone_Game_IV
+{
+    public class Solution
+    {
+        public bool WinnerSquareGame(int n)
+        {
+            bool[] dp = new bool[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j * j <= i; j++)
+                {
+                    if (!dp[i - j * j])
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
