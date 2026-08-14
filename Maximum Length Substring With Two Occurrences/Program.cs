837993633f2f9ee992b01693c3c3e25a23859d4f
phantom99a1 <viendanho99a1@gmail.com>
2026-08-14 19:57:07 +0700

    Add the problem Maximum Length Substring With Two Occurrences

---
+++ Maximum Length Substring With Two Occurrences/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Maximum_Length_Substring_With_Two_Occurrences
+{
+    public class Solution
+    {
+        public int MaximumLengthSubstring(string s)
+        {
+            int[] count = new int[26];
+            int left = 0;
+            int res = 0;
+            for (int right = 0; right < s.Length; right++)
+            {
+                int ch = s[right] - 'a';
+                count[ch]++;
+                while (count[ch] > 2)
+                {
+                    int ch2 = s[left] - 'a';
+                    count[ch2]--;
+                    left++;
+                }
+                res = Math.Max(res, right - left + 1);
+            }
+            return res;
+        }
+    }
+}
