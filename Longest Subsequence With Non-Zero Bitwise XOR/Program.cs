24fc3a90e9dabc727e63a8d4864c31704185a583
phantom99a1 <viendanho99a1@gmail.com>
2026-08-15 13:28:40 +0700

    Add the problem Longest Subsequence With Non-Zero Bitwise XOR

---
+++ Longest Subsequence With Non-Zero Bitwise XOR/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Longest_Subsequence_With_Non_Zero_Bitwise_XOR
+{
+
+    public class Solution
+    {
+        public int LongestSubsequence(int[] a)
+        {
+            int x = 0;
+            for (int i = 0; i < a.Length; i++) x ^= a[i];
+            if (x != 0) return a.Length;
+            for (int i = 0; i < a.Length; i++) if (a[i] != 0) return a.Length - 1;
+            return 0;
+        }
+    }
+}
