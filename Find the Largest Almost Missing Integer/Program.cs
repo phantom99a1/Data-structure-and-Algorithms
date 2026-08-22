5ecf63edbdee5f75876108f7077ebb631e1449c7
phantom99a1 <viendanho99a1@gmail.com>
2026-08-22 10:08:44 +0700

    Add the problem Find the Largest Almost Missing Integer

---
+++ Find the Largest Almost Missing Integer/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Find_the_Largest_Almost_Missing_Integer
+{
+    public class Solution
+    {
+        public int LargestInteger(int[] nums, int k) => nums.Length == k
+            ? nums.Max()
+            : k == 1
+                ? nums.GroupBy(m => m).
+                    Where(m => m.Count() <= 1).
+                    Select(n => n.Key).
+                    DefaultIfEmpty(-1).
+                    Max()
+                : Math.Max(
+                    nums.Count(m => m == nums[0]) <= 1 ? nums[0] : -1,
+                    nums.Count(m => m == nums[^1]) <= 1 ? nums[^1] : -1
+                    );
+    }
+}
