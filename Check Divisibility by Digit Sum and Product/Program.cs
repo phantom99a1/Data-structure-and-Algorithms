2c2920d612766a792a5c775b4333fad84c365076
phantom99a1 <viendanho99a1@gmail.com>
2026-08-22 10:05:03 +0700

    Add the problem Check Divisibility by Digit Sum and Product

---
+++ Check Divisibility by Digit Sum and Product/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Check_Divisibility_by_Digit_Sum_and_Product
+{
+    public class Solution
+    {
+        public bool CheckDivisibility(int n)
+        {
+            int digitSum = 0;
+            int digitProduct = 1;
+            int original = n;
+
+            while (n > 0)
+            {
+                int digit = n % 10;
+                n /= 10;
+
+                digitSum += digit;
+                digitProduct *= digit;
+            }
+
+            return original % (digitSum + digitProduct) == 0;
+        }
+    }
+}
