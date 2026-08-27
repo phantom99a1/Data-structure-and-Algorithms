aa21a77bb136a010600bc7fbb533d8042af8abba
phantom99a1 <viendanho99a1@gmail.com>
2026-08-27 19:16:10 +0700

    Add the problem Smallest Missing Multiple of K

---
+++ Smallest Missing Multiple of K/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Smallest_Missing_Multiple_of_K
+{
+    public class Solution
+    {
+        public int MissingMultiple(int[] nums, int k)
+        {
+            int temp = k;
+
+            while (nums.Contains(temp))
+            {
+                if (nums.Contains(temp))
+                {
+                    temp += k;
+                }
+            }
+            return temp;
+        }
+    }
+}
