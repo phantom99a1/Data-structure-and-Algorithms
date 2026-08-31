edddf83be575a758c8115311e8884d14173a2417
phantom99a1 <viendanho99a1@gmail.com>
2026-08-31 21:44:36 +0700

    Add the problem Find the Minimum and Maximum Number of Nodes Between Critical Points

---
+++ Find the Minimum and Maximum Number of Nodes Between Critical Points/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points
+{
+ public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+                 }
+  }
+
+    public class Solution
+    {
+        public int[] NodesBetweenCriticalPoints(ListNode head)
+        {
+            var pointer = head;
+
+            while (pointer.next != null)
+            {
+                if ((pointer.next?.val > pointer.val && pointer.next?.val > pointer.next?.next?.val) ||
+                    (pointer.next?.val < pointer.val && pointer.next?.val < pointer.next?.next?.val))
+                    break;
+
+                pointer = pointer.next;
+            }
+
+            if (pointer == null) return [-1, -1];
+
+            var max = 0;
+            var min = int.MaxValue;
+
+            var currentMin = 0;
+            var step = 0;
+
+            while (pointer.next != null)
+            {
+                if ((pointer.next?.val > pointer.val && pointer.next?.val > pointer.next?.next?.val) ||
+                    (pointer.next?.val < pointer.val && pointer.next?.val < pointer.next?.next?.val))
+                {
+                    max = step;
+                    if (currentMin > 0)
+                    {
+                        min = Math.Min(currentMin, min);
+                        currentMin = 0;
+                    }
+                }
+
+                pointer = pointer.next;
+                step++;
+                currentMin++;
+            }
+
+            if (max == 0 || min == int.MaxValue) return [-1, -1];
+
+            return [min, max];
+        }
+    }
+}
