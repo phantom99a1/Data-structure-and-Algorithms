9bb0ac35edc3a3b12c2f951fe754c6756e99dc93
phantom99a1 <viendanho99a1@gmail.com>
2026-09-03 22:40:12 +0700

    Add the problem Construct Uniform Parity Array II

---
+++ Construct Uniform Parity Array II/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Construct_Uniform_Parity_Array_II
+{
+    public class Solution
+    {
+        public bool UniformArray(int[] nums1)
+        {
+            int mn = nums1[0];
+            bool hasOdd = false;
+            foreach (int v in nums1)
+            {
+                if (v < mn)
+                {
+                    mn = v;
+                }
+                if ((v & 1) == 1)
+                {
+                    hasOdd = true;
+                }
+            }
+            if ((mn & 1) == 1)
+            {
+                return true;
+            }
+            return !hasOdd;
+        }
+    }
+}
