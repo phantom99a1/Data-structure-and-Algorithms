6771610bf8e08e9d74b34ad626db4ef18f18b84b
phantom99a1 <viendanho99a1@gmail.com>
2026-09-05 11:07:04 +0700

    Add the problem Smallest Stable Index II

---
+++ Smallest Stable Index II/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Smallest_Stable_Index_II
+{
+    class Solution
+    {
+        public int FirstStableIndex(int[] nums, int k)
+        {
+            int n = nums.Length;
+            int[] minValue = new int[n];
+            minValue[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                minValue[i] = Math.Min(minValue[i + 1], nums[i]);
+            }
+
+            int maxValue = 0;
+            for (int i = 0; i < n; i++)
+            {
+                maxValue = Math.Max(maxValue, nums[i]);
+                if (maxValue - minValue[i] <= k)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
