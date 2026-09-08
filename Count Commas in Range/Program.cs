1653a15b2b0818c84240612cf6700aca8879e077
phantom99a1 <viendanho99a1@gmail.com>
2026-09-08 22:45:56 +0700

    Add the problem Count Commas in Range

---
+++ Count Commas in Range/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Count_Commas_in_Range
+{
+    public class Solution
+    {
+        public int CountCommas(int n)
+        {
+            long totalCommas = 0;
+            long threshold = 1000;
+
+            while (n >= threshold)
+            {
+                totalCommas += (n - threshold + 1);
+                if (threshold > long.MaxValue / 1000) break;
+                threshold *= 1000;
+            }
+
+            return (int)totalCommas;
+        }
+    }
+}
