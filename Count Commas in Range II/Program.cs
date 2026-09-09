a9bfd8754a6974a45c5f55622501c86e432b0caf
phantom99a1 <viendanho99a1@gmail.com>
2026-09-09 23:52:45 +0700

    Add the problem Count Commas in Range II

---
+++ Count Commas in Range II/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Count_Commas_in_Range_II
+{
+    public class Solution
+    {
+        public long CountCommas(long n)
+        {
+            long p = 1000, res = 0;
+            while (p <= n)
+            {
+                res += n - p + 1;
+                p *= 1000;
+            }
+            return res;
+        }
+    }
+}
