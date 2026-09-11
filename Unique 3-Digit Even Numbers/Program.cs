864d6dd62814b96211e274b50f46f75b9a4d0f15
phantom99a1 <viendanho99a1@gmail.com>
2026-09-12 00:46:02 +0700

    Add the problem Unique 3-Digit Even Numbers

---
+++ Unique 3-Digit Even Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Unique_3_Digit_Even_Numbers
+{
+    public class Solution
+    {
+        public int TotalNumbers(int[] digits) =>
+            digits.SelectMany((a, i) =>
+                digits.Where((_, j) => j != i).
+                    SelectMany((b, j) => digits.Where((_, k) => k != i && k != (j >= i ? j + 1 : j)).
+                        Where(c => a != 0 && c % 2 == 0).
+            Select(c => a * 100 + b * 10 + c))).
+            Distinct().
+            Count();
+    }
+}
