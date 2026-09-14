da34672e1b6bd1bbb65e1b699321f03aab5a0ec3
phantom99a1 <viendanho99a1@gmail.com>
2026-09-14 23:37:25 +0700

    Add the problem Rectangle Overlap

---
+++ Rectangle Overlap/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Rectangle_Overlap
+{
+    public class Solution
+    {
+
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2)
+        {
+
+            bool flag = true;
+
+            if (
+                rec2[0] >= rec1[2] ||  // rec2 is to the right
+                rec1[3] <= rec2[1] ||  // rec1 is below
+                rec2[2] <= rec1[0] ||  // rec2 is to the left
+                rec2[3] <= rec1[1]     // rec2 is below
+            )
+            {
+                flag = false;
+            }
+
+            return flag;
+        }
+    }
+}
