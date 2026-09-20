c5ac30ccbf3d662459ce80860646fa35f7ed39b0
phantom99a1 <viendanho99a1@gmail.com>
2026-09-20 11:03:29 +0700

    Add the problem Reverse Degree of a String

---
+++ Reverse Degree of a String/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Reverse_Degree_of_a_String
+{
+    public class Solution
+    {
+        public int ReverseDegree(string s)
+        {
+            int ans = 0;
+            for (int i = 1; i <= s.Length; i++)
+            {
+                ans += (26 - (s[i - 1] - 'a')) * i;
+            }
+            return ans;
+        }
+    }
+}
