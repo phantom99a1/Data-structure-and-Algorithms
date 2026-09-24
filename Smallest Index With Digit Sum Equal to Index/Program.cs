c2fc3ef232c4a7197831da327eb6e52a2bc321ac
phantom99a1 <viendanho99a1@gmail.com>
2026-09-24 18:17:55 +0700

    Add the problem Smallest Index With Digit Sum Equal to Index

---
+++ Smallest Index With Digit Sum Equal to Index/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Smallest_Index_With_Digit_Sum_Equal_to_Index
+{
+    public class Solution
+    {
+        public int SmallestIndex(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int digitSum = 0;
+
+                while (num > 0)
+                {
+                    digitSum += num % 10;
+                    num /= 10;
+                }
+
+                if (digitSum == i)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
